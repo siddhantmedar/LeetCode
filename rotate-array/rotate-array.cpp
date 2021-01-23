015b6ec6a939da038459caf34a8d8c874aff8417
Siddhant Medar <sidmedar21@gmail.com>
2021-01-23 18:18:18 +0530

    Time: 4 ms (94.95%), Space: 10 MB (85.84%) - LeetHub

---
+++ rotate-array/rotate-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int result[nums.size()];
+        
+        for(int i=0; i < nums.size(); i++){
+            result[(i+k)%nums.size()] = nums[i];
+        }
+        
+        for(int i=0; i<nums.size(); i++){
+            nums[i] = result[i];
+        }
+        
+    }
+};
