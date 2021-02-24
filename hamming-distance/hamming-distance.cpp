55c53ac619506efafbbc0e4867e6b172201a1b5f
Siddhant Medar <sidmedar21@gmail.com>
2021-02-24 15:39:23 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.81%) - LeetHub

--- hamming-distance/hamming-distance.cpp
+++ hamming-distance/hamming-distance.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int hammingDistance(int x, int y) {
-        int n = x^y;
-        int count = 0;
-        while(n>0){
-            count += n&1;
-            n>>=1;
-        }
-        
-        return count;
-    }
+    int hammingDistance(int x, int y) {
+        return __builtin_popcount(x^y);
+    }
 };
