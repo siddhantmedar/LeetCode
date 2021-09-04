97df43e2be13b22d9f170c98285fc30c001184bc
Siddhant Medar <sidmedar21@gmail.com>
2021-09-04 02:05:13 -0500

    Time: 26 ms (11.75%), Space: 5.9 MB (73.69%) - LeetHub

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        string s = to_string(x);
-        int start=0, end = s.size()-1;
-        while(start<end){
-            if(s[start] != s[end]){
-                return false;
-            }
-            start++; end--;
-        }
-        return true;
-    }
+    bool isPalindrome(int x) {
+        string temp = to_string(x);
+        if(temp.size() == 0 || temp.size() == 1) return true;
+        int low=0, end=temp.size()-1;
+        while(low<end){
+            if(temp[low] != temp[end]) return false;
+            low++;end--;
+        }
+        return true;
+    }
 };
