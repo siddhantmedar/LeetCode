7f5ff2ad9db8127c9e6450641f40022c4eca11cc
Siddhant Medar <sidmedar21@gmail.com>
2021-10-02 14:22:37 -0500

    Time: 0 ms (100.00%), Space: 8.7 MB (85.68%) - LeetHub

--- plus-one/plus-one.cpp
+++ plus-one/plus-one.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& nums) {
-        int carry = 1;
-        
-        for(int i = nums.size()-1; i>=0; i--){
-            int sum = nums[i]+carry;
-            
-            if(sum>=10) carry = 1;
-            else carry = 0;
-            nums[i] = sum%10;
+        for(int i=nums.size()-1; i>=0; --i){
+            if(nums[i] == 9) nums[i] = 0;
+            else{
+                nums[i]++; return nums;
+            }
         }
-        
-        if(carry){
-            vector<int> result;
-            result.push_back(1);
-            for(auto v: nums)
-                result.push_back(v);
-            return result;
-        }
-        else return nums;
+        nums[0]=1; nums.emplace_back(0);
+        return nums;
     }
 };
