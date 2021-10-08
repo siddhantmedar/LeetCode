832100b6440f1c9b288c3bed731bd562f4ac6821
Siddhant Medar <sidmedar21@gmail.com>
2021-10-08 14:52:57 -0500

    Time: 0 ms (100.00%), Space: 40 MB (62.60%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int searchHelper(int[] nums, int start, int end, int target){
-        if(start <= end){
-        int mid = (start+end)/2;
-        if(nums[mid] == target) return mid;
-        else if(nums[mid] > target) return searchHelper(nums,start,mid-1,target);
-        else if(nums[mid] < target) return searchHelper(nums, mid+1, end, target);
+    public int helperBinarySearch(int[] nums, int start, int end, int target){
+        if(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) return helperBinarySearch(nums, start, mid-1, target);
+            else if(nums[mid] < target) return helperBinarySearch(nums, mid+1, end, target);
         }
         return -1;
     }
     public int search(int[] nums, int target) {
-        return searchHelper(nums, 0, nums.length-1, target);
+        return helperBinarySearch(nums, 0, nums.length-1, target);
     }
 }
