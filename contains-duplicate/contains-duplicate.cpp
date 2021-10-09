eecd94d1513f62011364f4fbacf9a00005e98baa
Siddhant Medar <sidmedar21@gmail.com>
2021-10-09 00:09:35 -0500

    Time: 16 ms (97.41%), Space: 15.6 MB (71.58%) - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(begin(nums), end(nums));
-        int flag = 0;
+        
         for(int i=0; i<nums.size()-1; ++i){
-            if(nums[i] == nums[i+1]){
-                flag=1; break;
-            }
+            if(nums[i] == nums[i+1]) return true;
         }
-        if(flag) return true;
-        else return false;
+        return false;
     }
 };
