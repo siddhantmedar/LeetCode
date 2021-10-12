512ba111a16fba0d20191b1e4126201d7ab59248
Siddhant Medar <sidmedar21@gmail.com>
2021-10-11 22:46:10 -0500

    Time: 54 ms (5.39%), Space: 6 MB (30.69%) - LeetHub

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x==0) return true;
-        int temp;
-        if(x < 0){
-            temp = abs(x);
-        }
-        else{
-            temp = x;
-        }
-        long long reverse = 0;
-        
+        long temp = abs(x);
+        long  rev = 0;
+
         while(temp > 0){
-            reverse = (reverse*10)+(temp%10);
+            rev =(rev*10)+(temp%10);
             temp/=10;
         }
-        if(x == reverse) return true;
-        return false;
+        return x == rev;
     }
 };
