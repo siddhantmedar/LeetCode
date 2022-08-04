bcc52215c08a70f01ba53a0bed945400018c2e74
Siddhant Medar <sidmedar21@gmail.com>
2022-08-04 18:03:58 -0500

    Time: 442 ms (44.66%), Space: 40.2 MB (10.97%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,54 +1,23 @@
-class ListNode:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.pre = None
-
 class MyHashMap:
-    def addNode(self, node):
-        ref = self.head.next
-        pre = ref.pre
-        
-        self.head.next = node
-        node.pre = self.head
-        
-        node.next = ref
-        ref.pre = node
-        
-    def removeNode(self, node):
-        pre = node.pre
-        nxt = node.next
-        
-        pre.next = nxt
-        nxt.pre = pre
-    
     def __init__(self):
         self.st = [None]*(10**6+1)
-        self.head = ListNode(-1,-1)
-        self.tail = ListNode(-1,-1)
-        
-        self.head.next = self.tail
-        self.tail.pre = self.head
         
     def put(self, key: int, value: int) -> None:
         if self.st[key]:
-            self.st[key].val = value
+            self.st[key][1] = value
         else:
-            self.st[key] = ListNode(key, value)
-            self.addNode(self.st[key])
+            self.st[key] = [key, value]
             
             
     def get(self, key: int) -> int:
         if self.st[key]:
-            return self.st[key].val
+            return self.st[key][1]
         else:
             return -1
         
 
     def remove(self, key: int) -> None:
         if self.st[key]:
-            self.removeNode(self.st[key])
             self.st[key] = None
             
         
