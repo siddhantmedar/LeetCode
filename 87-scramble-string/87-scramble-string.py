588e0109a6c69c32c748df059d52291a7ce4ff95
Siddhant Medar <sidmedar21@gmail.com>
2022-08-04 05:06:39 -0500

    Time: 85 ms (76.18%), Space: 14 MB (93.16%) - LeetHub

---
+++ 87-scramble-string/87-scramble-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        def isScrambled(x, y):
+            if len(x) != len(y):
+                return False
+
+            if (not len(x)) or (x == y):
+                return True
+
+            if sorted(x) != sorted(y):
+                return False
+
+            key = x + y
+            if key in dp:
+                print("used dp")
+                return dp[key]
+
+            for i in range(1, len(x)):
+                if (isScrambled(x[:i], y[:i]) and isScrambled(x[i:], y[i:])) or \
+                        (isScrambled(x[-i:], y[:i]) and isScrambled(x[:-i], y[i:])):
+                    dp[key] = True
+                    return True
+
+            dp[key] = False
+            return False
+
+
+        dp = {}
+        
+        return isScrambled(s1, s2)
+        
