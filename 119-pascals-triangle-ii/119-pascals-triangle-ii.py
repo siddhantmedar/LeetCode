5365931bc7ba094f4329c3cae186bb506e0fafd3
Siddhant Medar <sidmedar21@gmail.com>
2022-08-04 18:28:51 -0500

    Time: 46 ms (58.90%), Space: 13.9 MB (62.27%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getRow(self, n: int) -> List[int]:
+        dp = [[0]*i for i in range(1, n+2)]
+        
+        for i in range(len(dp)):
+            for j in range(len(dp[i])):
+                if i==0 and j==0:
+                    dp[i][j] = 1
+                    
+                elif i==0 or j==0:
+                    dp[i][j] = dp[i-1][j]
+                    
+                else:
+                    first = dp[i-1][j-1] if j-1 < len(dp[i-1]) else 0
+                    second = dp[i-1][j] if j < len(dp[i-1]) else 0
+                    
+                    dp[i][j] = first+second
+                    
+        return dp[-1]
+        
