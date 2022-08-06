0afc6a05c01eddbdddf091dd979fc11844241551
Siddhant Medar <sidmedar21@gmail.com>
2022-08-06 00:20:07 -0500

    Time: 459 ms (48.39%), Space: 14 MB (74.36%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -3,17 +3,6 @@ class Solution:
         def isValid(i,j):
             return 0<=i<N and 0<=j<N
 
-        def printMat(x,y):
-            for row in x:
-                print(row, end='')
-                print()
-
-            print("\n")
-
-            for row in y:
-                print(row, end='')
-                print()
-
         directions = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)]
 
         curr = [[0 for _ in range(N)] for _ in range(N)]
@@ -21,7 +10,7 @@ class Solution:
 
         curr[r][c] = 1
 
-        while k:
+        for m in range(k):
             next = [[0 for _ in range(N)] for _ in range(N)]
 
             for i in range(len(curr)):
@@ -36,7 +25,6 @@ class Solution:
                                 next[x][y] += curr[i][j]/8
                                 
             curr = next
-            k-=1
 
         return sum([sum(x) for x in curr])
         
