f30eead40597ca9f4dd00fc94a7a85b4d515db37
Siddhant Medar <sidmedar21@gmail.com>
2022-08-10 09:08:26 -0500

    Time: 171 ms (35.31%), Space: 15.6 MB (79.57%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,24 +1,47 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def DFS():
+            res = []
+            path = []
+            
+            def dfs(node):
+                path.append(node)
+                
+                if node == len(graph)-1:
+                    res.append(path.copy())
+                    path.remove(node)
+                    return
+                
+                for nei in mp[node]:
+                    dfs(nei)
+                    
+                path.remove(node)
+            
+            dfs(0)
+            
+            return res
+        
+        def BFS():
+            res = []
+            q = deque([(0,[0])])
+
+            while q:
+                k = len(q)
+
+                for i in range(k):
+                    node, path = q.popleft()
+
+                    if node == len(graph)-1:
+                        res.append(path)
+
+                    for nei in mp[node]:
+                        q.append((nei,path+[nei]))
+
+            return res
+        
         mp = defaultdict(list)
-        n = len(graph)
+        
         for i in range(len(graph)):
             mp[i].extend(graph[i])
             
-        res = []
-        
-        q = deque([(0,[0])])
-        
-        while q:
-            k = len(q)
-            
-            for i in range(k):
-                node, path = q.popleft()
-                
-                if node == n-1:
-                    res.append(path)
-                    
-                for nei in mp[node]:
-                    q.append((nei,path+[nei]))
-                  
-        return res
+        return DFS()
