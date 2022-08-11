fc964a1949f433646f39c38f15d72f0237362fb3
Siddhant Medar <sidmedar21@gmail.com>
2022-08-11 18:28:36 -0500

    Time: 1528 ms (97.05%), Space: 14.2 MB (59.20%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -7,17 +7,33 @@ class Solution:
         def confirm(i):
             for j in range(n):
                 if j!=i:
-                    if knows(i,j) or not knows(j,i):
+                    if (i,j) in dp:
+                        knows_else =  dp[(i,j)]
+                    else:
+                        knows_else = knows(i,j)
+                        dp[(i,j)] = knows_else
+                        
+                    if (j,i) in dp:
+                        known = dp[(j,i)]
+                        
+                    else:
+                        known = knows(j,i)
+                        dp[(j,i)] = known
+                    
+                    if knows_else or not known:
                         return False
+            
             return True
         
-        
+        dp = {}
         cand = 0
         
         for i in range(1,n):
-            if knows(cand,i):
+            result = knows(cand,i)
+            if result:
                 cand = i
-        
+            dp[(cand, i)] = result
+            
         return cand if confirm(cand) else -1       
             
         
