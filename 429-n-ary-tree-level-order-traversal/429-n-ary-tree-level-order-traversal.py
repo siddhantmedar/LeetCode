a15545a74a1014cb978cd6e8719aee675122be91
Siddhant Medar <sidmedar21@gmail.com>
2022-08-13 03:09:55 -0500

    Time: 65 ms (76.07%), Space: 16.5 MB (11.19%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -8,24 +8,46 @@ class Node:
 
 class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
-        if not root:
-            return []
-        
-        res = []
-        
-        q = deque([root])
+        def BFS():
+            if not root:
+                return []
 
-        while q:
-            n = len(q)
-            tmp = []
+            res = []
+
+            q = deque([root])
+
+            while q:
+                n = len(q)
+                tmp = []
+
+                for i in range(n):
+                    node = q.popleft()
+                    tmp.append(node.val)
+
+                    for c in node.children:
+                        q.append(c)
+
+                res.append(tmp)
+
+            return res
+        
+        # return BFS()
+    
+        def DFS(root):
+            mp = {i:[] for i in range(1, 1001)}
             
-            for i in range(n):
-                node = q.popleft()
-                tmp.append(node.val)
+            def dfs(root, level):
+                if not root:
+                    return
                 
-                for c in node.children:
-                    q.append(c)
-                    
-            res.append(tmp)
+                mp[level].append(root.val)
+                
+                for c in root.children:
+                    dfs(c,level+1)
+                
+            dfs(root, 1)
             
-        return res
+            return [v for k,v in mp.items() if v]
+                
+        
+        return DFS(root)
