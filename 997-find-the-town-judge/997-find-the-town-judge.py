2657facb2d34771488aa25cb2103216be7a9db39
Siddhant Medar <sidmedar21@gmail.com>
2022-08-13 04:31:46 -0500

    Time: 1407 ms (19.66%), Space: 18.9 MB (65.83%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -5,17 +5,20 @@ class Solution:
         
         mp = {i:(0,0) for i in range(1, 1001)}
         
+        st = set([x for x in range(1,n+1)])
+        
         for u,v in trust:
             degree = mp[u]
             mp[u] = (degree[0],degree[1]+1)
-            
+                
             degree = mp[v]
             mp[v] = (degree[0]+1,degree[1])
             
-        res = None
+            if u in st:
+                st.remove(u)
         
-        for k,v in mp.items():
-            if v[0] == n-1 and v[1] == 0:
-                res = k
-            
-        return res if res else -1
+        if st and len(st) == 1:
+            cand = [x for x in st][-1]
+            return  cand if mp[cand][0] == n-1 and mp[cand][1] == 0 else -1
+        
+        return -1
