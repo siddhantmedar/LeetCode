4885246a6320c5393689b101bb02d43d75cf5ead
Siddhant Medar <sidmedar21@gmail.com>
2022-08-12 21:22:03 -0500

    Time: 92 ms (73.62%), Space: 26.2 MB (79.55%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,18 +7,42 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return root
+        def getPath(root, target, path):
+            nonlocal found
+            
+            if not root:
+                return
+            
+            path.append(root)
+            
+            if root == target:
+                found = True
+                return
+            
+            getPath(root.left, target, path)
+            
+            if found:
+                return
+            
+            getPath(root.right, target, path)
+            
+            if found:
+                return
+            
+            path.pop()
+            
+        path1, path2 = [], []
         
-        if root == p or root == q:
-            return root
+        found = False
+        getPath(root, p, path1)
         
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        found = False
+        getPath(root, q, path2)
         
-        if left and right:
-            return root
+        result = None
         
-        if left or right:
-            return left if left else right
-        
+        for i in range(min(len(path1), len(path2))):
+            if path1[i] == path2[i]:
+                result = path1[i]
+                
+        return result
