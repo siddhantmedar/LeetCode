74de211f5949ad3c5dabcc30ae5607d627a6d43a
Siddhant Medar <sidmedar21@gmail.com>
2022-08-13 02:18:23 -0500

    Time: 69 ms (65.16%), Space: 14.4 MB (85.68%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def bfs(root):           
+            q = deque([root])
+            
+            while q:
+                n = len(q)
+                tmp = []
+                for i in range(n):
+                    node = q.popleft()
+                    tmp.append(node)
+                    
+                    if node.left:
+                        q.append(node.left)
+                        
+                    if node.right:
+                        q.append(node.right)
+                        
+                lst = tmp
+            
+            return lst
+        
+        def lca(root):
+            if not root or (root in st):
+                return root
+
+            left = lca(root.left)
+            right = lca(root.right)
+
+            if left and right:
+                return root
+
+            return left or right
+
+        st = set(bfs(root))
+        return lca(root)
