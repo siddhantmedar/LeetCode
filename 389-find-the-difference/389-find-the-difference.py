e8fc1978f1ad7d0a07669ff632345c69a9de7acd
Siddhant Medar <sidmedar21@gmail.com>
2022-08-14 19:01:33 -0500

    Time: 26 ms (99.56%), Space: 13.8 MB (75.84%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,21 +1,30 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        mp = {}
-        res = None
+#         mp = {}
+#         res = None
+        
+#         for c in s:
+#             mp[c] = 1+mp.get(c,0)
+            
+#         for c in t:
+#             if c in mp:
+#                 mp[c]-=1
+#             else:
+#                 res = c
+        
+#         for k,v in mp.items():
+#             if v:
+#                 res = k
+        
+        res = 0
         
         for c in s:
-            mp[c] = 1+mp.get(c,0)
-            
+            res^=ord(c)
+        
         for c in t:
-            if c in mp:
-                mp[c]-=1
-            else:
-                res = c
-        
-        for k,v in mp.items():
-            if v:
-                res = k
-        
+            res^=ord(c)
+            
+        return chr(res)
         return res
             
         
