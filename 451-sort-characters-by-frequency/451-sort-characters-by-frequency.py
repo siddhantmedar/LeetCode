855528cb364a1c8ad8f2a45f387131b1efc95b28
Siddhant Medar <sidmedar21@gmail.com>
2022-08-15 22:27:12 -0500

    Time: 6576 ms (5.05%), Space: 50.5 MB (5.05%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,21 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        mp = {x:0 for x in s}
+        
+        for c in s:
+            mp[c]+=1
+            
+        bucket = [[] for i in range(1,500001)]
+        
+        for k,v in mp.items():
+            bucket[v].append(k)
+            
+        res = ""
+        for i in range(len(bucket)-1,-1,-1):
+            b = bucket[i]
+            
+            if b:
+                for ele in b:
+                    res+=(i*ele)
+                    
+        return res
