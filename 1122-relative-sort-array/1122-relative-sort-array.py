7588b05f55c8fa9762c3633403b48f03cf656381
Siddhant Medar <sidmedar21@gmail.com>
2022-08-15 20:34:16 -0500

    Time: 62 ms (51.26%), Space: 14 MB (77.96%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         mp = {x:0 for x in arr2}
+        container = [0]*1001
         
         res = []
         misc = []
@@ -9,11 +10,16 @@ class Solution:
             if ele in mp:
                 mp[ele] = 1+mp.get(ele,0)
             elif ele not in mp:
-                misc.append(ele)
+                container[ele]+=1
                 
         for k,v in mp.items():
             while v:
                 res.append(k)
                 v-=1
-                
-        return res+sorted(misc)
+        
+        for ele, count in enumerate(container):
+            while count:
+                misc.append(ele)
+                count-=1
+    
+        return res+misc
