77ab79d6d8358fb87710e44332014775f4529655
Siddhant Medar <sidmedar21@gmail.com>
2022-08-28 11:01:42 -0500

    Time: 143 ms (89.86%), Space: 16.6 MB (81.80%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,6 +1,5 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @lru_cache
         def solve(i,j):
             if i == 0:
                 return j
@@ -8,16 +7,24 @@ class Solution:
             elif j == 0:
                 return i
             
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
             val = 1
             
             if word1[i-1] == word2[j-1]:
                 val = 0
-                return solve(i-1,j-1) + val
+                dp[i][j] = solve(i-1,j-1) + val
+                return dp[i][j]
             
             else:
-                return min(solve(i,j-1),
+                dp[i][j] = min(solve(i,j-1),
                           solve(i-1,j-1),
                           solve(i-1,j)
                           ) + val
             
+                return dp[i][j]
+            
+        dp = [[-1 for _ in range(len(word2)+1)] for _ in range(len(word1)+1)]
+        
         return solve(len(word1), len(word2))
