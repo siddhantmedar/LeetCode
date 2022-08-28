94e0f088c76ef68a5c255308bb55200d3d476657
Siddhant Medar <sidmedar21@gmail.com>
2022-08-28 10:52:25 -0500

    Time: 38 ms (92.99%), Space: 13.8 MB (99.09%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -2,12 +2,11 @@ class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         pattern = {}
         
-        for idx, c in enumerate(order):
-            pattern[c] = idx
+        for i, c in enumerate(order):
+            pattern[c] = i
             
         for i in range(len(words)-1):
             word1, word2 = words[i], words[i+1]
-            
             mn = min(len(word1), len(word2))
             
             if len(word1) > len(word2) and word1[:mn] == word2[:mn]:
