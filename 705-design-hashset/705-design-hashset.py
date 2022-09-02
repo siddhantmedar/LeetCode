6e5640864614f5433eac16c89c88091b25b08779
Siddhant Medar <sidmedar21@gmail.com>
2022-09-02 11:49:22 -0500

    Time: 412 ms (42.39%), Space: 20.6 MB (22.87%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,18 +1,61 @@
-class MyHashSet:
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        
+class Bucket:
     def __init__(self):
-        self.lst = []
+        self.head = Node(-1)
+    
+    def nodeExists(self, key):
+        tmp = self.head
+
+        while tmp:
+            if tmp.val == key:
+                return True
+
+            tmp = tmp.next
+
+        return False
+
+    def addNode(self, key):
+        if not self.nodeExists(key):
+            newNode = Node(key)
+            newNode.next = self.head.next
+            self.head.next = newNode
+    
+    def removeNode(self, key):
+        if self.nodeExists(key):
+            pre = self.head
+            curr = self.head.next
+            
+            while curr:
+                if curr.val == key:
+                    pre.next = curr.next
+                    return
+                
+                pre = curr
+                curr = curr.next
+        
+class MyHashSet:
+
+    def __init__(self):
+        self.lst = [Bucket() for _ in range(769)]
+    
+    def _hash(self, key):
+        return key % 769
 
     def add(self, key: int) -> None:
-        if key not in self.lst:
-            self.lst.append(key)
-        
+        idx = self._hash(key)
+        self.lst[idx].addNode(key)
+
     def remove(self, key: int) -> None:
-        if key in self.lst:
-            self.lst.remove(key)
+        idx = self._hash(key)
+        self.lst[idx].removeNode(key)
 
     def contains(self, key: int) -> bool:
-        return key in self.lst
-
+        idx = self._hash(key)
+        return self.lst[idx].nodeExists(key)
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
