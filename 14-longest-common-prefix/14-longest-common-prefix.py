744515d6608f011aaed60e0d706c9d595b476e3e
Siddhant Medar <sidmedar21@gmail.com>
2022-09-05 11:49:48 -0500

    Time: 65 ms (27.10%), Space: 14 MB (49.91%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,42 +1,19 @@
-class Trie:
-    def __init__(self, c=""):
-        self.children = {}
-        self.isEnd = False
-        
-    @staticmethod
-    def insertWords(strs):
-        root = Trie()
-        
-        for word in strs:
-            node = root
-            
-            for c in word:
-                if c not in node.children:
-                    node.children[c] = Trie(c)
-                node = node.children[c]
-            
-            node.isEnd = True
-            
-        return root
-            
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if "" in strs:
-            return ""
-        
-        root = Trie.insertWords(strs)
+        word = min(strs)
         
         res = ""
         
-        node = root
-        
-        while len(node.children) == 1 and not node.isEnd:
-            ch = None
+        for i, c in enumerate(word):
+            count = 0
+            for w in strs:
+                if w[i] == c:
+                    count+=1
+                    
+            if count == len(strs):
+                res+=c
             
-            for k,v in node.children.items():
-                res+=k
-                ch = k
+            else:
+                break
             
-            node = node.children[ch]
-        
         return res
