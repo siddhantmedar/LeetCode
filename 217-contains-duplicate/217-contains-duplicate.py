868d8441be6d688e8d1d3740014407bdb30ea15f
Siddhant Medar <sidmedar21@gmail.com>
2022-09-19 07:49:26 -0500

    Time: 1194 ms (5.03%), Space: 25.8 MB (89.54%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        '''
         st = set()
         
         for ele in nums:
@@ -7,5 +8,14 @@ class Solution:
                 return True
             
             st.add(ele)
+            
+        return False 
+        '''
+        
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
             
         return False
