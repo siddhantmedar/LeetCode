ff5b2f0a8e75f27e0ac019bed1a4e72750651f76
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 18:24:08 -0500

    Time: 57 ms (51.37%), Space: 13.9 MB (61.69%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,41 +1,45 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         def toposort():
-            count = 0
-            res = ""
+            result = []
             q = deque([k for k,v in indegree.items() if v == 0])
             
             while q:
-                n = len(q)
-                for k in range(n):
-                    node = q.popleft()
-                    res+=node
-                    count+=1
-                    
-                    if node in graph:
-                        for nei in graph[node]:
-                            indegree[nei]-=1
-                            if indegree[nei] == 0:
-                                q.append(nei)
-        
-            return res if count == len(indegree) else ""
-        
+                node = q.popleft()
+                result.append(node)
+                
+                if node in graph:
+                    for nei in graph[node]:
+                        indegree[nei]-=1
+
+                        if indegree[nei] == 0:
+                            q.append(nei)
+            
+            return result if len(result) == len(indegree) else []
+                
         graph = defaultdict(set)
         indegree = {c:0 for word in words for c in word}
         
         for i in range(len(words)-1):
-            word1, word2 = words[i], words[i+1]
-            mn = min(len(word1), len(word2))
-            if len(word1) > len(word2) and word1[:mn] == word2[:mn]:
+            x,y = words[i], words[i+1]
+            
+            l = min(len(x), len(y))
+            
+            if (len(x) > len(y) and y[:l] == x[:l]):
                 return ""
             
-            for j in range(mn):
-                if word1[j] != word2[j]:
-                    if word2[j] not in graph[word1[j]]:
-                        graph[word1[j]].add(word2[j])
-                        indegree[word2[j]] = 1+indegree.get(word2[j],0)
+            for j in range(l):
+                w1 = x[j]
+                w2 = y[j]
+                
+                if w1 != w2:
+                    if w2 not in graph[w1]:
+                        indegree[w2] = 1+indegree.get(w2, 0)
+                    
+                    graph[w1].add(w2)
+                    
                     break
         
         result = toposort()
-        
-        return result
+        print(graph)
+        return "".join(result)
