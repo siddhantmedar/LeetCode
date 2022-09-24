dde0a900a349ed961bbef541e40535396304376f
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 16:49:39 -0500

    Time: 286 ms (8.65%), Space: 15.4 MB (86.77%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -11,11 +11,22 @@ class Solution:
             setY = find(y)
             
             if setX != setY:
-                parent[setY] = setX
-        
-        parent = {x:x for x in range(n)}
+                if size[setX] >= size[setY]:
+                    size[setX]+=size[setY]
+                    parent[setY] = setX
+                    
+                else:
+                    size[setY]+=size[setX]
+                    parent[setX] = setY
+                    
+        parent = {i:i for i in range(n)}
+        size = {i:1 for i in range(n)}
         
         for u,v in edges:
             union(u,v)
         
-        return sum([1 for k,v in parent.items() if k == v])
+        print(parent)
+        print(size)
+        
+        return sum([1 for k,v in parent.items() if k ==  v])
+    
