99d13b75377714531d3de896ad27d72a2ceee43d
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 16:52:15 -0500

    Time: 184 ms (47.22%), Space: 17.3 MB (16.07%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,32 +1,26 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-                
-            return parent[x]
-        
-        def union(x,y):
-            setX = find(x)
-            setY = find(y)
+        def dfs(node):
+            visited.add(node)
             
-            if setX != setY:
-                if size[setX] >= size[setY]:
-                    size[setX]+=size[setY]
-                    parent[setY] = setX
-                    
-                else:
-                    size[setY]+=size[setX]
-                    parent[setX] = setY
-                    
-        parent = {i:i for i in range(n)}
-        size = {i:1 for i in range(n)}
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        
+        graph = defaultdict(list)
         
         for u,v in edges:
-            union(u,v)
+            graph[u].append(v)
+            graph[v].append(u)
         
-        print(parent)
-        print(size)
+        visited = set()
         
-        return sum([1 for k,v in parent.items() if k ==  v])
-    
+        count = 0
+        
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count+=1
+                
+        return count
