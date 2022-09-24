0b4362e6e018020ae0129f5421bc8c679d92ce0f
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 00:13:31 -0500

    Time: 308 ms (72.59%), Space: 22.4 MB (18.20%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -22,8 +22,6 @@ class Solution:
             
         for i in range(len(nums)-2, -1, -1):
             right[i] = right[i+1]*nums[i+1]
-            
-        # print(left, right)
         
         result = []
         
