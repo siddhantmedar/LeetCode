7aa50bdc8c3fc61aa230f2e24bec2798243abba9
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 18:58:17 -0500

    Time: 102 ms (27.10%), Space: 14.3 MB (85.57%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,20 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def isPalin(x):
-            start, end = 0, len(x)-1
-            
-            while start <= end:
-                if x[start] != x[end]:
-                    return False
-                start+=1
-                end-=1
-                
-            return True
-            
-        res = ""
+        t = ""
         
         for c in s:
             if c.isalnum():
-                res+=c.lower()
-                
-        return isPalin(res)
+                t+=c.lower()
+        
+        start, end = 0, len(t)-1
+        
+        while start <= end:
+            if t[start] != t[end]:
+                return False
+            
+            start+=1
+            end-=1
+            
+        return True
+        
