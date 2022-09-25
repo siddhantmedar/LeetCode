9fe6e1b09f6d9d4016c6423e746812d1449352ee
Siddhant Medar <sidmedar21@gmail.com>
2022-09-25 17:46:24 -0500

    Time: 892 ms (59.72%), Space: 36.6 MB (27.05%) - LeetHub

---
+++ 148-sort-list/148-sort-list.py
@@ -0,0 +1,68 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def middle(head):
+            slow, fast = head, head.next
+            
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+                
+            return slow
+        
+        def merge(l,r):
+            if not l:
+                return r
+            
+            if not r:
+                return l
+            
+            dummy = ListNode(-1)
+            tmp = dummy
+            
+            while l and r:
+                if l.val <= r.val:
+                    tmp.next = l
+                    tmp = tmp.next
+                    l = l.next
+                    
+                else:
+                    tmp.next = r
+                    tmp = tmp.next
+                    r = r.next
+                    
+            while l:
+                tmp.next = l
+                tmp = tmp.next
+                l = l.next
+                
+            while r:
+                tmp.next = r
+                tmp = tmp.next
+                r = r.next
+                
+            return dummy.next
+        
+        def mergeSort(head):
+            if not head or not head.next:
+                return head
+            
+            mid = middle(head)
+            
+            l = head
+            r = mid.next
+            
+            mid.next = None
+            
+            left = mergeSort(l)
+            right = mergeSort(r)
+            
+            result = merge(left, right)
+            
+            return result
+        
+        return mergeSort(head)
