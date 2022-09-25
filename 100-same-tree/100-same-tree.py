a067746acffba3c9b2848fd6b0e36f9e4084fb8a
Siddhant Medar <sidmedar21@gmail.com>
2022-09-24 19:54:02 -0500

    Time: 69 ms (6.96%), Space: 13.9 MB (75.77%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,35 +6,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def dfs(p,q):
-            if not p and not q:
-                return True
-
-            if (p and not q) or (q and not p):
-                return False
-
-            return p.val == q.val and dfs(p.left, q.left) and dfs(p.right, q.right)
-    
-        def bfs(p,q):
-            q = deque([(p,q)])
-            
-            while q:
-                node1, node2 = q.popleft()
-                
-                if (node1 and not node2) or (node2 and not node1) or\
-                    (node1 and node2 and (node1.val != node2.val)):
-                    return False
-                
-                if (node1 and node1.left) or (node2 and node2.left):
-                    q.append((node1.left if node1 else None, node2.left if node2 else None))
-                
-                if (node1 and node1.right) or (node2 and node2.right):
-                    q.append((node1.right if node1 else None, node2.right if node2 else None))
-            
+        if not p and not q:
             return True
         
-        return bfs(p,q)
-                
-                
-                
-                
+        if (not p and q) or (p and not q):
+            return False
+        
+        return p.val == q.val and self.isSameTree(p.left, q.left)\
+        and self.isSameTree(p.right, q.right)
