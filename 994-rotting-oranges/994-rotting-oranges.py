cee9c1e273209bcf034ae633beb9713b47d34662
Siddhant Medar <sidmedar21@gmail.com>
2022-09-25 20:06:12 -0500

    Time: 115 ms (20.32%), Space: 14 MB (11.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,5 +1,55 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        def inplace_bfs():
+            def solve():
+                nonlocal fresh
+                
+                updated = False
+                
+                for i in range(M):
+                    for j in range(N):
+                        if grid[i][j] == timestamp:
+                            for dx, dy in directions:
+                                dx+=i
+                                dy+=j
+                                
+                                if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 1:
+                                    grid[dx][dy] = timestamp+1    
+                                    fresh-=1
+                                    updated = True
+                                    
+                                    if fresh == 0:
+                                        return time
+                
+                return updated
+            
+            M,N = len(grid), len(grid[0])
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]
+
+            fresh = 0
+
+            for i in range(M):
+                for j in range(N):
+                    if grid[i][j] == 1:
+                        fresh+=1
+
+            timestamp, time = 2, 0
+
+            if fresh == 0:
+                return time
+
+
+            while True:
+                time+=1
+                result = solve()
+                timestamp+=1
+                
+                if fresh == 0:
+                    return time
+                
+                if not result:
+                    return -1
+                
         def bfs():
             M,N = len(grid), len(grid[0])
             fresh = 0
@@ -41,7 +91,8 @@ class Solution:
             
             return time if fresh == 0 else -1
         
-        return bfs()
+        return inplace_bfs()
+        # return bfs()
         
         
         
