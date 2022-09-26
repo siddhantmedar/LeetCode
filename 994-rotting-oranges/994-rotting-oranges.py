92513a8c8476e5520395d207be06ffc71adc506f
Siddhant Medar <sidmedar21@gmail.com>
2022-09-25 19:50:09 -0500

    Time: 90 ms (51.11%), Space: 13.9 MB (91.54%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,45 +1,137 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def bfs(timer):
-            nonlocal fresh
+        def bfs():
+            M,N = len(grid), len(grid[0])
+            fresh = 0
             
-            changed = False
+            q = deque()
             
             for i in range(M):
                 for j in range(N):
-                    if grid[i][j] == timer:
-                        
-                        for dx, dy in directions:
-                            dx+=i
-                            dy+=j
-                            
-                            if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 1:
-                                grid[dx][dy] = timer+1
-                                fresh-=1
-                                changed = True
-                   
-            return changed
-                        
-        
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
-        
-        M,N = len(grid), len(grid[0])
-        
-        count = 0
-        fresh = 0
-        timer = 1
-        
-        for i in range(M):
-            for j in range(N):
-                if grid[i][j] == 1:
-                    fresh+=1
-        
-        while fresh:
-            count+=1
-            timer+=1
+                    if grid[i][j] == 2:
+                        q.append((i,j))
+                    elif grid[i][j] == 1:
+                        fresh+=1
             
-            if not bfs(timer):
-                break
+            time = 0
+            
+            if fresh == 0:
+                return time
+            
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]
+            
+            while q:
+                n = len(q)
+                time+=1
+                
+                for k in range(n):
+                    i, j = q.popleft()
+                    
+                    for dx, dy in directions:
+                        dx+=i
+                        dy+=j
+                        
+                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 1:
+                            q.append((dx,dy))
+                            grid[dx][dy] = 2
+                            fresh-=1
+                            
+                            if fresh == 0:
+                                return time
+            
+            return time if fresh == 0 else -1
         
-        return count if fresh == 0 else -1
+        return bfs()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
