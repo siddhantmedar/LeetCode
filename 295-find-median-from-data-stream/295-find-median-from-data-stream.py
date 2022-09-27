71c2e7fd6b49f85d94d517869fce139d543bc8b3
Siddhant Medar <sidmedar21@gmail.com>
2022-09-27 01:27:45 -0500

    Time: 1564 ms (18.17%), Space: 35.9 MB (71.49%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,23 +1,23 @@
 class MedianFinder:
 
     def __init__(self):
-        self.mx = []
-        self.mn = []
+        self.mx_heap = []
+        self.mn_heap = []
         
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.mx, num)
+        heapq.heappush(self.mx_heap, -1*num)
         
-        heapq.heappush(self.mn, -1*heapq.heappop(self.mx))
+        heapq.heappush(self.mn_heap, -1*heapq.heappop(self.mx_heap))
         
-        while len(self.mn) > len(self.mx):
-            heapq.heappush(self.mx, -1*heapq.heappop(self.mn))
-        
+        while len(self.mn_heap) > len(self.mx_heap):
+            heapq.heappush(self.mx_heap, -1*heapq.heappop(self.mn_heap))
+
     def findMedian(self) -> float:
-        if len(self.mx) > len(self.mn):
-            return self.mx[0]
+        if len(self.mx_heap) == len(self.mn_heap):
+            return ((-1*self.mx_heap[0]) + self.mn_heap[0]) / 2
         
-        else:
-            return (self.mx[0]+(-1*self.mn[0]))/2
+        elif len(self.mx_heap) > len(self.mn_heap):
+            return -1*self.mx_heap[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
