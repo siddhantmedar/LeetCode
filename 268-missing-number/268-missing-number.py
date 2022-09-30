7426907a7ac78454c0695c42f655ca106e83afc9
Siddhant Medar <sidmedar21@gmail.com>
2022-09-29 19:58:05 -0500

    Time: 349 ms (22.79%), Space: 16.4 MB (6.66%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,12 +1,3 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        result = 0
-        
-        for ele in range(0, len(nums)+1):
-            result^=ele
-            
-        for ele in nums:
-            result^=ele
-            
-        return result
-        
+        return list(set([x for x in range(0, len(nums)+1)]).difference(set(nums)))[0]
