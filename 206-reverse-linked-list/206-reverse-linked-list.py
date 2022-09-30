6b8a8742f9781aaf34d615b1e35c3775239397fd
Siddhant Medar <sidmedar21@gmail.com>
2022-09-29 21:47:24 -0500

    Time: 70 ms (32.74%), Space: 15.4 MB (55.01%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,8 +5,16 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # def reverseRec(head)
-        
+        def reverseRec(head):
+            if not head or not head.next:
+                return head
+            
+            result = reverseItr(head.next)
+            
+            head.next.next = head
+            head.next = None
+            
+            return result
         
         def reverseItr(head):
             pre, curr = None, head
@@ -21,4 +29,6 @@ class Solution:
             
             return pre
         
-        return reverseItr(head)
+        # return reverseItr(head)
+        
+        return reverseRec(head)
