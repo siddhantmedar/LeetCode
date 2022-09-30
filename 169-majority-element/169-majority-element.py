a0568e73d043e15bae13f17d968f40ba284a1e9e
Siddhant Medar <sidmedar21@gmail.com>
2022-09-30 03:01:42 -0500

    Time: 505 ms (5.02%), Space: 15.6 MB (34.08%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,11 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        cand, count = nums[0], 1
+        cand = nums[0]
+        count = 1
         
         for ele in nums[1:]:
             if ele == cand:
                 count+=1
-                
+                continue
+            
             else:
                 count-=1
                 
