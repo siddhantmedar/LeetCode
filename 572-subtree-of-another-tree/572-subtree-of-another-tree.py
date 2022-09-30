d6df01ca41c5f76f4eeda84b746fe29d7efcd7c9
Siddhant Medar <sidmedar21@gmail.com>
2022-09-30 02:10:27 -0500

    Time: 315 ms (11.30%), Space: 15 MB (85.61%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -10,16 +10,19 @@ class Solution:
             if not p and not q:
                 return True
             
-            if (not p and q) or (p and not q):
+            if (p and not q) or (not p and q):
                 return False
             
-            return p.val==q.val and same(p.left, q.left) and same(p.right,q.right)
+            return p.val == q.val and same(p.left, q.left) and same(p.right, q.right)
         
         def dfs(root):
             if not root:
                 return
             
-            return same(root, subRoot) or dfs(root.left) or dfs(root.right)
+            if same(root, subRoot):
+                return True
+            
+            return dfs(root.left) or dfs(root.right)
+            
         
         return dfs(root)
-        
