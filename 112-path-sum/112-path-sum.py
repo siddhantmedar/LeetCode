a9b779ee11c7fa92b9bae308d75048ae63159102
Siddhant Medar <sidmedar21@gmail.com>
2022-09-30 00:29:07 -0500

    Time: 94 ms (16.55%), Space: 15.1 MB (16.04%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -20,5 +20,28 @@ class Solution:
             return solve(root.left, rem-root.val) or solve(root.right, rem-root.val)
         
         return solve(root, targetSum)
-            
+        
+        def solve2(root, rem):
+            q = deque([(root, targetSum)])
+
+            while q:
+                n = len(q)
+
+                for k in range(n):
+                    node, rem = q.popleft()
+
+                    if not node.left and not node.right:
+                        if rem-node.val == 0:
+                            return True
+
+                    if node.left:
+                        q.append((node.left, rem-root.val))
+
+                    if node.right:
+                        q.append((node.right, rem-root.val))
+
+            return False
+
+        
+        return solve2(root, targetSum)
         
