5b18bca0410a7a186491d581fe541509922b8ced
Siddhant Medar <sidmedar21@gmail.com>
2022-09-29 20:27:47 -0500

    Time: 66 ms (8.22%), Space: 13.9 MB (11.77%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -16,13 +16,14 @@ class Solution:
         # return solve1(n)
         
         def solve2():
-            dp = [0 for _ in range(n+1)]
-            
-            dp[0], dp[1] = 1, 1
+            prepre, pre = 1, 1
             
             for i in range(2, n+1):
-                dp[i] = dp[i-1] + dp[i-2]
+                curr = pre + prepre
                 
-            return dp[n]
+                prepre = pre
+                pre = curr
+            
+            return pre
         
         return solve2()
