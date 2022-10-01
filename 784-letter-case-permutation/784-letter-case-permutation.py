13aa58ba17c602589558630296f7deac47eec250
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 13:48:19 -0500

    Time: 150 ms (13.94%), Space: 15.3 MB (39.01%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,18 +1,28 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        def solve(idx, res):
-            if idx >= len(s):
-                result.append(res)
+        def solve(idx):
+            if idx == len(s):
+                result.append("".join(lst))
                 return
-            
+                
             if s[idx].isalpha():
-                solve(idx+1, res+s[idx].upper())
-                solve(idx+1, res+s[idx].lower())
+                lst.append(s[idx].lower())
+                solve(idx+1)
+                lst.pop()
+                
+                lst.append(s[idx].upper())
+                solve(idx+1)
+                lst.pop()
+                
             else:
-                solve(idx+1, res+s[idx])
-            
+                lst.append(s[idx])
+                solve(idx+1)
+                lst.pop()
+                
+        
+        lst = deque()
         result = []
         
-        solve(0,"")
+        solve(0)
         
         return result
