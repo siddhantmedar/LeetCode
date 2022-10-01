a8228bbc16b0ca4f932b322c27a016879e0b9387
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 14:29:40 -0500

    Time: 54 ms (74.62%), Space: 14.2 MB (23.21%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def solve(idx):
+            if idx == len(nums)-1:
+                tmp.append(nums[idx])
+                result.append(tmp[:])
+                tmp.pop()
+                return
+            
+            for i in range(idx, len(nums)):
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+                tmp.append(nums[idx])
+
+                solve(idx+1)
+
+                tmp.pop()
+
+                nums[idx], nums[i] = nums[i], nums[idx]
+        
+        result = []
+        tmp = []
+        
+        solve(0)
+        
+        return result
+        
