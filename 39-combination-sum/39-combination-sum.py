d5285daf32ddbb53b2014bd0badd934f06eb1786
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 14:46:19 -0500

    Time: 266 ms (8.66%), Space: 14 MB (85.87%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def solve(idx, sm, res):
+            if sm == target:
+                if res not in result:
+                    result.append(res[:])
+                
+                return
+            
+            if idx>=len(nums) or sm > target:
+                return
+            
+            solve(idx, sm+nums[idx], res+[nums[idx]])
+            solve(idx+1, sm, res)
+        
+        result = []
+        
+        nums.sort()
+        solve(0, 0, [])
+        
+        return result
