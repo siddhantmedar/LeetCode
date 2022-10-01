f0fe545f08c9d5128866639cf8d807353e92a6d5
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 11:33:03 -0500

    Time: 253 ms (41.73%), Space: 14.8 MB (54.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,34 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row, col = False, False
-        M, N = len(matrix), len(matrix[0])
+        M,N = len(matrix), len(matrix[0])
+        row, col = set(), set()
         
-        for j in range(N):
-            if matrix[0][j] == 0:
-                row = True
-                
         for i in range(M):
-            if matrix[i][0] == 0:
-                col = True
-        
-        for i in range(1, M):
-            for j in range(1, N):
-                if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    matrix[i][0] = 0
-        
-        for i in range(1, M):
-            for j in range(1, N):
-                if matrix[0][j] == 0 or matrix[i][0] == 0:
-                    matrix[i][j] = 0
-        
-        if row:
             for j in range(N):
-                matrix[0][j] = 0
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    col.add(j)
+                    
+        for i in range(M):
+            for j in range(N):
+                if i in row or j in col:
+                    matrix[i][j] = 0
+                    
         
-        if col:
-            for i in range(M):
-                matrix[i][0] = 0
-        
+                    
         
