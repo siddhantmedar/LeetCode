481436d5f73fecc0e40c8fb8395173021b737bcd
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 17:12:55 -0500

    Time: 224 ms (12.10%), Space: 36.8 MB (5.01%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,35 +8,16 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-
-        def middle(head):
-            slow, fast = head, head.next
-            
-            while fast and fast.next:
-                slow = slow.next
-                fast = fast.next.next
-                
-            return slow
-        
-        def reverse(head):
-            if not head or not head.next:
-                return head
-            
-            result = reverse(head.next)
-            
-            head.next.next = head
-            head.next = None
-            
-            return result
-        
         def merge(l,r):
             if not l:
                 return r
             
             if not r:
-                return l
+                return l 
             
-            dummy = ListNode(-1)
+            dummy = ListNode(-101)
+            dummy.next = head
+            
             tmp = dummy
             
             while l and r:
@@ -52,22 +33,53 @@ class Solution:
                 tmp.next = l
                 tmp = tmp.next
                 l = l.next
-
+                
             while r:
                 tmp.next = r
                 tmp = tmp.next
                 r = r.next
-
+                
             return dummy.next
+        
+        def reverse1(head):
+            pre, curr = None, head
             
+            while curr:
+                ref = curr.next
+                curr.next = pre
+                pre = curr
+                curr = ref
             
+            return pre
+        
+        def reverse2(head):
+            if not head or not head.next:
+                return head
+            
+            result = reverse2(head.next)
+            
+            head.next.next = head
+            head.next = None
+            
+            return result
+            
+        def middle(head):
+            slow, fast = head, head.next
+            
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+                
+            return slow
+        
         mid = middle(head)
-
-        l = head
-        r = mid.next
+        
+        left = head
+        right = mid.next
         
         mid.next = None
         
-        rev = reverse(r)
+        right = reverse2(right)
         
-        return merge(l, rev)
+        return merge(left, right)
+        
