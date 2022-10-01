68ad5a4cc14fa3a2ad3e065dccf8132cfac90b20
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 12:19:40 -0500

    Time: 66 ms (34.50%), Space: 14 MB (29.35%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,23 +1,24 @@
 class Solution:
-    def rotate(self, grid: List[List[int]]) -> None:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        N = len(matrix)
         
-        l, r = 0, len(grid)-1
+        left, right = 0, N-1
         
-        while l < r:
-            for i in range(r-l):
-                t, b = l, r
+        while left < right:
+            for i in range(right-left):
+                top, bottom = left, right
+                
+                tmp = matrix[top][left+i]
+                
+                matrix[top][left+i] = matrix[bottom-i][left]
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                matrix[bottom][right-i] = matrix[top+i][right]
+                matrix[top+i][right] = tmp
+                
+            left+=1
+            right-=1
             
-                tmp = grid[t][l+i]
-                
-                grid[t][l+i] = grid[b-i][l]
-                grid[b-i][l] = grid[b][r-i]
-                grid[b][r-i] = grid[t+i][r]
-                grid[t+i][r] = tmp
-                
-            l+=1
-            r-=1
-        
         
