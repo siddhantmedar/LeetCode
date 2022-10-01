1c005eb8dbc3e77efb4f550892fbdbc9c186c6e7
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 17:22:12 -0500

    Time: 300 ms (8.83%), Space: 19.1 MB (32.59%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,15 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = dict()
+        mp = defaultdict(list)
         
         for word in strs:
             container = [0]*26
             
             for c in word:
                 container[ord(c)-ord('a')]+=1
-                
-            if tuple(container) not in mp:
-                mp[tuple(container)] = []
 
             mp[tuple(container)].append(word)    
         
