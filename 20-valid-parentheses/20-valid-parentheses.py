97ad3e9d1304086689569562e71a74f327abafd1
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 19:48:38 -0500

    Time: 47 ms (66.13%), Space: 13.9 MB (72.19%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,27 +5,27 @@ class Solution:
         for c in s:
             if c == "(" or c == "[" or c == "{":
                 st.append(c)
-            
+                
             else:
                 if not st:
                     return False
-                
+
                 if c == ")":
                     if st and st[-1] == "(":
                         st.pop()
                     else:
                         return False
+                    
                 elif c == "]":
                     if st and st[-1] == "[":
                         st.pop()
                     else:
                         return False
-                elif c == "}":
+                    
+                if c == "}":
                     if st and st[-1] == "{":
                         st.pop()
                     else:
                         return False
                     
         return True if not st else False
-                    
-        
