2247ea98c55278d9f396f3f22199a547daad2bff
Siddhant Medar <sidmedar21@gmail.com>
2022-10-02 18:41:05 -0500

    Time: 1327 ms (5.01%), Space: 15.7 MB (27.76%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,6 +1,18 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        arr.sort(key=lambda z:abs(z-x))
+        @cmp_to_key
+        def cmp(a,b):
+            if abs(a-x) < abs(b-x):
+                return -1
+            
+            elif abs(a-x) == abs(b-x):
+                return 0
+            
+            elif abs(a-x) > abs(b-x):
+                return 1
+            
+            
+        arr.sort(key=cmp)
         
         print(arr)
         
