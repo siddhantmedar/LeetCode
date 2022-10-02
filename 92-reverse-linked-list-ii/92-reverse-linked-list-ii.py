d9b6a94c3ab78c93ba7de20f05d5dbe466f8d39e
Siddhant Medar <sidmedar21@gmail.com>
2022-10-02 12:55:02 -0500

    Time: 33 ms (93.11%), Space: 14.4 MB (8.26%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], l: int, r: int) -> Optional[ListNode]:
+        def solve(right):
+            nonlocal left, l, r, stop
+            
+            if l != 1:
+                l-=1
+                left = left.next
+                
+            if r != 1:
+                r-=1
+                solve(right.next)
+                
+            if left == right or right.next == left:
+                stop = True
+            
+            if not stop:
+                left.val, right.val = right.val, left.val
+                left = left.next
+            
+            
+        left = head
+        stop = False
+        
+        solve(head)
+        
+        return head
