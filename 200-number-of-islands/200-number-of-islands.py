9dc7192585443fc6c8cd33f936993f19998ecc55
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 23:17:00 -0500

    Time: 1051 ms (6.08%), Space: 25.3 MB (9.90%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,39 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def find(x,y):
-            if parent[(x,y)] != (x,y):
-                parent[(x,y)] = find(parent[(x,y)][0], parent[(x,y)][1])
+        def dfs(i,j):
+            if i<0 or i>=M or j<0 or j>=N or (i,j) in visited or grid[i][j] == "0":
+                return False
             
-            return parent[(x,y)]
-        
-        def union(i,j,x,y):
-            setX = find(i,j)
-            setY = find(x,y)
+            visited.add((i,j))
             
-            if setX != setY:
-                if size[(i,j)] >= size[(x,y)]:
-                    parent[setY] = setX
-                    size[setX]+=size[setY]
-                else:
-                    parent[setX] = setY
-                    size[setY]+=size[setX]
+            for dx, dy in directions:
+                dx+=i
+                dy+=j
+                
+                dfs(dx, dy)
         
         M,N = len(grid), len(grid[0])
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        visited = set()
         
-        parent = {(i,j):(i,j) for i in range(M) for j in range(N) if grid[i][j] == "1"}
-        size = {(i,j):1 for i in range(M) for j in range(N) if grid[i][j] == "1"}
+        count = 0
         
         for i in range(M):
             for j in range(N):
-                if grid[i][j] == "1":
-                    for dx, dy in directions:
-                        dx+=i
-                        dy+=j
-                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == "1":
-                            union(i,j,dx,dy)
-        
-        print(parent)
-        print(size)
-        return sum([1 for k,v in parent.items() if k == v])
-        
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    dfs(i,j)
+                    count+=1
+                    
+        return count
