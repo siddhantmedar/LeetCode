f0c009f8d5426c5bb1dcb932d89431b458801f4f
Siddhant Medar <sidmedar21@gmail.com>
2022-10-01 20:17:32 -0500

    Time: 306 ms (5.08%), Space: 14 MB (16.02%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,21 +1,24 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        def rev(n):
-            nonlocal s
-            
-            if n > 0:
-                s+=str(n%10)
-                rev(n//10)
-            
         if x < 0:
             return False
         
-        if x == 0:
-            return True
+        def solve1(n):
+            if n>0:
+                tmp = n%10
+                solve1(n//10)   
+                lst1.append(tmp)
+
+        def solve2(n):
+            if n>0:
+                lst2.append(n%10)
+                solve2(n//10)
         
-        s=""
+        lst1, lst2 = [], []
         
-        rev(x)
+        solve1(x)
+        solve2(x)
         
-        return int(s) == x
+        # print(lst1, lst2)
         
+        return lst1 == lst2
