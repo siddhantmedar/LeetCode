34d10063ed587c0e9c40d3aa3f472ba45eba7f49
Siddhant Medar <sidmedar21@gmail.com>
2022-10-03 00:30:48 -0500

    Time: 1020 ms (5.00%), Space: 14 MB (18.63%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,26 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        i = 0
-        mp = dict()
-        
         result = 0
+        mp = {}
+        
+        i = 0
         
         for j in range(len(s)):
-            mp[s[j]] = 1+mp.get(s[j], 0)
+            ele = s[j]
+            mp[ele] = 1+mp.get(ele, 0)
             
-            mx = max([v for _, v in mp.items()])
-            
-            if (j-i+1) - mx <= k:
-                result = max(result, j-i+1)
+            if (j-i+1)-max([v for _,v in mp.items()]) <= k:
+                result = max(result,j-i+1)
                 
             else:
-                while (j-i+1) - mx > k:
+                while (j-i+1)-max([v for _,v in mp.items()]) > k:
                     mp[s[i]]-=1
+                    
+                    if mp[s[i]] == 0:
+                        del mp[s[i]]
+                    
                     i+=1
-            
-        if len(s)-i <= k:
-            result = max(result, len(s)-i)
-            
-        print(result)
         
+        if (len(s)-i)-max([v for _,v in mp.items()]) <= k:
+            result = max(result,len(s)-i)
+            
         return result
