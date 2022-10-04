b9a878ebf74bb48e7330980289ce424f76043375
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 11:57:24 -0500

    Time: 556 ms (13.48%), Space: 18.7 MB (94.78%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,25 +6,24 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def search(x):
-            for i, ele in enumerate(inorder):
+        def getIdx(ele):
+            for i,x in enumerate(inorder):
                 if ele == x:
                     return i
-                
-        
+            
         def solve(sp, ep, si, ei):
             if sp > ep or si > ei:
                 return None
             
             root = TreeNode(preorder[sp])
             
-            idx = search(preorder[sp])
-            count = idx-si
-                
-            root.left = solve(sp+1,sp+count, si, idx-1)
+            idx = getIdx(preorder[sp])
             
-            root.right = solve(sp+count+1, ep, idx+1, ei)
+            root.left = solve(sp+1,sp+(idx-si),si,idx-1)
+        
+            root.right = solve(sp+(idx-si)+1,ep,idx+1, ei)
             
             return root
         
-        return solve(0, len(preorder)-1, 0, len(inorder)-1)
+        
+        return solve(0,len(preorder)-1,0,len(inorder)-1)
