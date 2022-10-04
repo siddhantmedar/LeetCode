cdc04e57b5fb5faf59d800854f75c4349721f266
Siddhant Medar <sidmedar21@gmail.com>
2022-10-03 21:58:18 -0500

    Time: 305 ms (9.36%), Space: 15.5 MB (88.86%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findOrder(self, n: int, edges: List[List[int]]) -> List[int]:
+        def bfs():
+            result = []
+            
+            q = deque([k for k,v in indegree.items() if v == 0])
+
+            while q:
+                nn = len(q)
+                
+                for k in range(nn):
+                    node = q.popleft()
+                    result.append(node)
+                    
+                    for nei in graph[node]:
+                        indegree[nei]-=1
+                        
+                        if indegree[nei] == 0:
+                            q.append(nei)
+            
+            return result if len(result) == n else []
+        
+        graph = defaultdict(list)
+        indegree = {i:0 for i in range(n)}
+        
+        for v,u in edges:
+            graph[u].append(v)
+            indegree[v] = 1+indegree.get(v,0)
+            
+        return bfs()
