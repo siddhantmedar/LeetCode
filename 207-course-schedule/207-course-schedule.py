014726aee98d42695e0df27e8b10587993b5f5ac
Siddhant Medar <sidmedar21@gmail.com>
2022-10-03 21:48:31 -0500

    Time: 107 ms (89.98%), Space: 17.5 MB (9.87%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,64 +1,34 @@
 class Solution:
     def canFinish(self, n: int, edges: List[List[int]]) -> bool:
-        def DFS():
-            def dfs(node):
-                visited.add(node)    
-                rec.append(node)
-
-                for nei in graph[node]:
-                    if nei not in visited:
-                        if not dfs(nei):
-                            return False
-
-                    elif nei in rec:
+        def dfs(node):
+            visited.add(node)
+            rec.add(node)
+            
+            for nei in graph[node]:
+                if nei not in visited:
+                    if not dfs(nei):
                         return False
-
-                rec.remove(node)
-                return True
-
-
-            graph = defaultdict(list)
-            visited = set()
-            rec = []
-
-            for u, v in edges:
-                graph[u].append(v)
-
-            for i in range(n):
-                if i not in visited:
-                    if not dfs(i):
-                        return False
+                elif nei in rec:
+                    return False
+                
+            rec.remove(node)
+            st.append(node)
+            
             return True
+        
+        
+        graph = defaultdict(list)
+        
+        for v,u in edges:
+            graph[u].append(v)
             
-        def BFS():
-            graph = defaultdict(list)
-            
-            visited = set()
-            indegree = {x:0 for x in range(n)}
-            
-            for u, v in edges:
-                graph[u].append(v)
-                indegree[v] = 1 + indegree.get(v, 0)
+        visited = set()
+        rec = set()
+        st = []
+        
+        for i in range(n):
+            if i not in visited:
+                if not dfs(i):
+                    return False
                 
-            q = deque()
-            
-            for k,v in indegree.items():
-                if v == 0:
-                    q.append(k)
-            
-            while q:
-                node = q.popleft()
-                visited.add(node)
-                
-                for nei in graph[node]:
-                    indegree[nei]-=1
-                    
-                    if indegree[nei] == 0:
-                        q.append(nei)
-            
-            print(visited)
-            return len(visited) == n
-        
-        
-        return BFS()
-        
+        return True
