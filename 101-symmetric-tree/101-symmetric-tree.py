6e84516138aea030ca06975cebd2a9435a49c411
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 01:59:32 -0500

    Time: 75 ms (12.56%), Space: 14 MB (21.48%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,23 +6,13 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        q = deque([(root, root)])
-        
-        while q:
-            n = len(q)
+        def solve(root1, root2):
+            if not root1 and not root2:
+                return True
             
-            for i in range(n):
-                node1, node2 = q.popleft()
-                
-                if (node1 and not node2) or (node2 and not node1):
-                    return False
-                
-                if node1.val != node2.val:
-                    return False
-                
-                if node1.left or node2.right:
-                    q.append((node1.left if node1 else None, node2.right if node2 else None))
-                if node1.right or node2.left:
-                    q.append((node1.right if node1 else None, node2.left if node2 else None))
-                             
-        return True
+            if (not root1 and root2) or (root1 and not root2):
+                return False
+            
+            return root1.val == root2.val and solve(root1.left, root2.right) and solve(root1.right, root2.left)
+        
+        return solve(root, root)
