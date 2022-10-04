9e6d80e964d94a593685d5205dee666027e227b5
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 15:39:55 -0500

    Time: 1481 ms (32.74%), Space: 15.2 MB (48.47%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,27 +1,34 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        directions = [(-1,0), (1,0), (0,-1), (0,1), (-1,-1), (-1,1), (1,-1), (1,1)]
-        visited = set()
         M,N = len(grid), len(grid[0])
+        directions = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(-1,1),(1,-1),(1,1)]
+        
+        q = deque()
+        visited = set()
         
         if grid[0][0] == 0:
+            q.append((0,0))
             visited.add((0,0))
-            q = deque([(0,0)])
-            depth = 0 
         
-            while q:
-                n = len(q)
-                depth+=1
-                for i in range(n):
-                    i,j = q.popleft()
-
-                    if i == M-1 and j == N-1:
-                        return depth
-
-                    for dx,dy in directions:
-                        if 0<=i+dx<M and 0<=j+dy<N and (i+dx, j+dy) not in visited\
-                        and grid[i+dx][j+dy] == 0:
-                            visited.add((i+dx, j+dy))
-                            q.append((i+dx,j+dy))
-                        
+        level = 0
+        
+        while q:
+            n = len(q)
+            level+=1
+            
+            for k in range(n):
+                i,j = q.popleft()
+                
+                if i == M-1 and j == N-1:
+                    return level
+                
+                for dx, dy in directions:
+                    dx+=i
+                    dy+=j
+                    
+                    if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 0 and (dx,dy) not in visited:
+                        visited.add((dx,dy))
+                        q.append((dx,dy))
+        
         return -1
+        
