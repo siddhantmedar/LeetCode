71f730113688dd736133db1bdb671d78881b3a61
Siddhant Medar <sidmedar21@gmail.com>
2022-10-03 21:56:57 -0500

    Time: 146 ms (69.49%), Space: 15.6 MB (77.13%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,34 +1,32 @@
 class Solution:
     def canFinish(self, n: int, edges: List[List[int]]) -> bool:
-        def dfs(node):
-            visited.add(node)
-            rec.add(node)
+        def bfs():
+            count = 0
+            result = []
             
-            for nei in graph[node]:
-                if nei not in visited:
-                    if not dfs(nei):
-                        return False
-                elif nei in rec:
-                    return False
+            q = deque([k for k,v in indegree.items() if v == 0])
+
+            while q:
+                nn = len(q)
                 
-            rec.remove(node)
-            st.append(node)
+                for k in range(nn):
+                    node = q.popleft()
+                    count+=1
+                    result.append(node)
+                    
+                    for nei in graph[node]:
+                        indegree[nei]-=1
+                        
+                        if indegree[nei] == 0:
+                            q.append(nei)
             
-            return True
-        
+            return True if len(result) == n else False
         
         graph = defaultdict(list)
+        indegree = {i:0 for i in range(n)}
         
         for v,u in edges:
             graph[u].append(v)
+            indegree[v] = 1+indegree.get(v,0)
             
-        visited = set()
-        rec = set()
-        st = []
-        
-        for i in range(n):
-            if i not in visited:
-                if not dfs(i):
-                    return False
-                
-        return True
+        return bfs()
