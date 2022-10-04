a963139024e99072b2c380ed6789451dbe82c2b5
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 15:30:42 -0500

    Time: 1899 ms (16.90%), Space: 17.9 MB (19.90%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,31 +1,33 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         M,N = len(mat), len(mat[0])
-        directions = [(-1,0), (1,0), (0,-1),(0,1)]
-        
-        dist = [[0 for _ in range(N)] for _ in range(M)]
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
         
         q = deque()
+        result = [[float("inf") for _ in range(N)] for _ in range(M)]
         
         for i in range(M):
             for j in range(N):
                 if mat[i][j] == 0:
-                    dist[i][j] = 0
+                    result[i][j] = 0
+                    
                     q.append((i,j))
-                elif mat[i][j] == 1:
-                    dist[i][j] = float("inf")
+                    
         
         while q:
             n = len(q)
             
-            for i in range(len(q)):
+            for k in range(n):
                 i,j = q.popleft()
                 
-                for x,y in directions:
-                    x+=i
-                    y+=j
-                    if 0<=x<M and 0<=y<N and dist[x][y] > dist[i][j]+1:
-                        dist[x][y] = dist[i][j]+1
-                        q.append((x,y))
+                for dx, dy in directions:
+                    dx+=i
+                    dy+=j
+                    
+                    if 0<=dx<M and 0<=dy<N and result[dx][dy] > 1 + result[i][j]:
+                        result[dx][dy] = 1+result[i][j]
+                        q.append((dx,dy))
                         
-        return dist
+        print(result)
+        
+        return result
