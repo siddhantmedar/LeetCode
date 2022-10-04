005f3a754f11b537fbb50f84eee7ba66a12a2704
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 15:09:16 -0500

    Time: 190 ms (8.79%), Space: 14.2 MB (38.81%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -11,20 +11,35 @@ class Solution:
                 dy+=j
                 
                 dfs(dx,dy,src,fill)
+                
+        
+        def bfs():
+            src = grid[sr][sc]
+            fill = color
+            
+            q = deque([(sr,sc)])
+            grid[sr][sc] = fill
+            
+            while q:
+                n = len(q)
+                
+                for k in range(n):
+                    i,j = q.popleft()
+
+                    for dx, dy in directions:
+                        dx+=i
+                        dy+=j
+                        
+                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == src and grid[dx][dy] != fill:
+                            grid[dx][dy] = fill
+                            q.append((dx,dy))
             
         
         M,N = len(grid), len(grid[0])
         
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
         
-        dfs(sr, sc, grid[sr][sc], color)
+        # dfs(sr, sc, grid[sr][sc], color)
+        bfs()
         
         return grid
-    
-    
-    
-#      [0,0,0]
-#      [2,2,2]
-# 1
-# 0
-# 2
