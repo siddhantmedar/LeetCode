97df708d958083f93c3a5ef40906c5cb27a6021d
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 21:53:09 -0500

    Time: 1913 ms (17.15%), Space: 27.6 MB (11.22%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,16 +1,9 @@
 class Solution:
     def maxArea(self, nums: List[int]) -> int:
-#         result = 0
-        
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 result = max(result, (j-i)*min(nums[i], nums[j]))
-                
-#         return result
-
-        result = 0
         left, right = 0, len(nums)-1
-    
+        
+        result = 0
+        
         while left < right:
             result = max(result, (right-left)*min(nums[left], nums[right]))
             
@@ -21,5 +14,3 @@ class Solution:
                 right-=1
                 
         return result
-                
-                
