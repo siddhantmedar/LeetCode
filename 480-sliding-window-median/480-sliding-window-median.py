3b860166dcbe95bdaadfa2edbb57ff8844bee655
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 19:31:04 -0500

    Time: 2259 ms (21.92%), Space: 15.8 MB (77.96%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.py
+++ 480-sliding-window-median/480-sliding-window-median.py
@@ -1,39 +1,44 @@
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         def add(ele):
-            heapq.heappush(mx_heap, -1*ele)
+            heapq.heappush(mx, -1*ele)
             
-            heapq.heappush(mn_heap, -1*heapq.heappop(mx_heap))
-        
-            while len(mn_heap) > len(mx_heap):
-                heapq.heappush(mx_heap, -1*heapq.heappop(mn_heap))
+            heapq.heappush(mn, -1*heapq.heappop(mx))
             
-        def median():
-            if len(mx_heap) == len(mn_heap):
-                return ((mx_heap[0]*-1) + (mn_heap[0])) / 2
+            while len(mn) > len(mx):
+                heapq.heappush(mx, -1*heapq.heappop(mn))
             
-            elif len(mx_heap) > len(mn_heap):
-                return mx_heap[0]*-1
+        def getMedian():
+            if len(mx) > len(mn):
+                return -1*mx[0]
             
-        def updateHeap(ele):
-            if -1*mx_heap[0] >= ele and -1*ele in mx_heap:
-                mx_heap.remove(-1*ele)
-                heapq.heapify(mx_heap)
             else:
-                mn_heap.remove(ele)
-                heapq.heapify(mn_heap)
+                return (-1*mx[0] + mn[0]) / 2
+            
+        def updateHeaps(ele):
+            if ele <= -1*mx[0]:
+                mx.remove(-1*ele)
+                heapq.heapify(mx)
+                
+            else:
+                mn.remove(ele)
+                heapq.heapify(mn)
         
-        mx_heap, mn_heap = list(), list()
+        
+        mn, mx = [], []
         
         result = []
         
-        for i, ele in enumerate(nums):
-            if len(mx_heap) + len(mn_heap) < k:
-                add(ele)
-                
-            if len(mx_heap) + len(mn_heap) == k:
-                result.append(median())
-                
-                updateHeap(nums[i-k+1])
-                
+        for i in range(k):
+            add(nums[i])
+            
+        result.append(getMedian())
+        
+        for i in range(k, len(nums)):
+            updateHeaps(nums[i-k])
+            
+            add(nums[i])
+            
+            result.append(getMedian())
+            
         return result
