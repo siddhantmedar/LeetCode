f482dbd471f5b01b4481e41b62d121683a0a7ebb
Siddhant Medar <sidmedar21@gmail.com>
2022-10-05 13:00:12 -0500

    Time: 2007 ms (25.76%), Space: 74.4 MB (95.77%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -4,73 +4,75 @@ class Node:
         self.val = val
         self.pre = None
         self.next = None
-
+        
+        self.mp = dict()
+        
 class LRUCache:
-    def addNode(self, key, val):
-        node = Node(key, val)
-        
-        ref = self.head.next
-        
-        node.pre = self.head
-        node.next = ref
-        
-        self.head.next = node
-        ref.pre = node
-        
-        return node
-        
-    def deleteNode(self, node):
-        preref = node.pre
-        nxtref = node.next
-        
-        preref.next = nxtref
-        nxtref.pre = preref
-        
+
     def __init__(self, capacity: int):
+        self.capacity = capacity
         self.mp = {}
-        self.cap = capacity
-        self.size = 0
         
         self.head = Node(-1,-1)
         self.tail = Node(-1,-1)
         
         self.head.next = self.tail
         self.tail.pre = self.head
-
+    
+        
+    def addNode(self, node):
+        ref = self.head.next
+        
+        self.head.next = node
+        
+        node.pre = self.head
+        node.next = ref
+        
+        ref.pre = node
+        
+    def removeNode(self, node):
+        pre = node.pre
+        nxt = node.next
+        
+        pre.next = nxt
+        nxt.pre = pre
+        
     def get(self, key: int) -> int:
         if key in self.mp:
             result = self.mp[key].val
             
-            value = self.mp[key].val
-            
-            self.deleteNode(self.mp[key])
+            self.removeNode(self.mp[key])
             del self.mp[key]
             
-            self.mp[key] = self.addNode(key,value)
+            node = Node(key, result)
+            
+            self.addNode(node)
+            self.mp[key] = node
             
             return result
-        
+            
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.mp:
-            self.deleteNode(self.mp[key])
+            self.removeNode(self.mp[key])
             del self.mp[key]
             
-            self.mp[key] = self.addNode(key, value)
+            node = Node(key,value)
+            self.mp[key] = node
+            
+            self.addNode(node)
             
         else:
-            if self.size == self.cap:
-                k = self.tail.pre.key
-                self.deleteNode(self.tail.pre)
-                del self.mp[k]
-                self.size-=1
-            
-            self.mp[key] = self.addNode(key,value)
-            
-            self.size+=1
-
+            if len(self.mp) == self.capacity:
+                rm = self.tail.pre.key
+                self.removeNode(self.tail.pre)
+                del self.mp[rm]
+                
+            node = Node(key,value)
+            self.mp[key] = node
+            self.addNode(node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
