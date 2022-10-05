1dd6b7c87013ba1064e16c469e3277d4b5bdd135
Siddhant Medar <sidmedar21@gmail.com>
2022-10-05 01:45:19 -0500

    Time: 63 ms (43.52%), Space: 14 MB (28.55%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,45 +1,46 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         def toposort():
-            result = []
             q = deque([k for k,v in indegree.items() if v == 0])
             
+            result = ""
+            
             while q:
-                node = q.popleft()
-                result.append(node)
+                n = len(q)
                 
-                if node in graph:
+                for k in range(n):
+                    node = q.popleft()
+                    result+=node
+                    
                     for nei in graph[node]:
                         indegree[nei]-=1
-
+                        
                         if indegree[nei] == 0:
                             q.append(nei)
+                            
+            return result if len(result) == len(indegree) else ""
             
-            return result if len(result) == len(indegree) else []
-                
         graph = defaultdict(set)
         indegree = {c:0 for word in words for c in word}
-        
+            
         for i in range(len(words)-1):
             x,y = words[i], words[i+1]
             
-            l = min(len(x), len(y))
+            mn = min(len(x), len(y))
             
-            if (len(x) > len(y) and y[:l] == x[:l]):
+            if len(x) > len(y) and x[:mn] == y[:mn]:
                 return ""
             
-            for j in range(l):
-                w1 = x[j]
-                w2 = y[j]
-                
-                if w1 != w2:
-                    if w2 not in graph[w1]:
-                        indegree[w2] = 1+indegree.get(w2, 0)
+            for i in range(mn):
+                if x[i] != y[i]:
                     
-                    graph[w1].add(w2)
+                    if y[i] not in graph[x[i]]:
+                        indegree[y[i]] = 1+indegree.get(y[i],0)
+                        # indegree[x[i]] = indegree.get(x[i],0)
+                    
+                    graph[x[i]].add(y[i])
                     
                     break
         
-        result = toposort()
-
-        return "".join(result)
+        return toposort()
+            
