be591f587f04d41a34879afb88f6d419f9455d81
Siddhant Medar <sidmedar21@gmail.com>
2022-10-04 23:58:35 -0500

    Time: 1112 ms (30.10%), Space: 14.7 MB (87.94%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,38 +1,34 @@
 class Solution:
-    def longestIncreasingPath(self, nums: List[List[int]]) -> int:
-        def dfs(i,j,x,y):
-            if i<0 or i>=M or j<0 or j>=N:
+    def longestIncreasingPath(self, grid: List[List[int]]) -> int:
+        def dfs(i,j):
+            if i<0 or j<0 or i>=M or j>=N:
                 return 0
             
-            if (i,j) in visited:
+            if dp[i][j] != -1:
                 return dp[i][j]
             
-            visited.add((i,j))
-            
-            res = 0
+            result = 0
             
             for dx, dy in directions:
-                if 0<=dx+i<M and 0<=dy+j<N and nums[dx+i][dy+j] > nums[i][j]:
-                    res = max(res, dfs(dx+i, dy+j, i, j))
+                dx+=i
+                dy+=j
+                
+                if 0<=dx<M and 0<=dy<N and grid[dx][dy] > grid[i][j]:
+                    result = max(result, dfs(dx,dy))
             
-            dp[i][j] = max(res+1, dp[i][j])
+            dp[i][j] = max(dp[i][j], result+1)
             
             return dp[i][j]
         
         
-        M,N = len(nums), len(nums[0])
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
-        visited = set()
+        M,N = len(grid), len(grid[0])
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        dp = [[-1 for _ in range(N)] for _ in range(M)]
         
-        res = float("-inf")
-        
-        dp = [[1 for _ in range(N)] for _ in range(M)]
+        result = 0
         
         for i in range(M):
             for j in range(N):
-                result = dfs(i,j,i,j)
+                result = max(result, dfs(i,j))
                 
-                res = max(res, result)
-                
-        return res
-                
+        return result
