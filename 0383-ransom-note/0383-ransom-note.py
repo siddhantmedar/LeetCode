5714e9b14b75e93152ab03c3ddecb92e90822d4d
Siddhant Medar <sidmedar21@gmail.com>
2022-11-11 22:39:02 -0600

    Time: 88 ms (70.88%), Space: 14 MB (93.97%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mp = {}
+        
+        for c in magazine:
+            mp[c] = 1+mp.get(c,0)
+        
+        for c in ransomNote:
+            if c not in mp:
+                return False
+            
+            else:
+                mp[c]-=1
+                
+                if mp[c] == 0:
+                    del mp[c]
+                    
+        return True
