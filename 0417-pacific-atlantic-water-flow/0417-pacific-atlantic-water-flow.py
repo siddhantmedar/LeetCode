5f4f66e536089dca67ebdfae1b070d3e3e635f09
Siddhant Medar <sidmedar21@gmail.com>
2022-11-12 13:28:42 -0600

    Time: 677 ms (55.15%), Space: 15.7 MB (56.00%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,29 +1,39 @@
 class Solution:
     def pacificAtlantic(self, nums: List[List[int]]) -> List[List[int]]:
-        def dfs(i,j,pre,st):
-            if i<0 or i>=M or j<0 or j>=N or (i,j) in st or nums[i][j] < pre:
-                return
+        def solve():
+            def bfs(i,j,pre,st):
+                q = deque([(i,j,pre,st)])
+                
+                while q:
+                    n = len(q)
+                    
+                    for k in range(n):
+                        i,j,pre,st = q.popleft()
+                        if i<0 or i>=M or j<0 or j>=N or (i,j) in st or nums[i][j] < pre:
+                                continue
+                        
+                        st.add((i,j))
+                        
+                        for dx,dy in directions:
+                            dx+=i
+                            dy+=j
+                            
+                            q.append((dx,dy,nums[i][j], st))
             
-            st.add((i,j))
             
-            for dx,dy in directions:
-                dx+=i
-                dy+=j
-            
-                dfs(dx,dy,nums[i][j],st)
-            
-            
-        M,N = len(nums), len(nums[0])
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+            M,N = len(nums), len(nums[0])
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]
+
+            pac, atl = set(), set()
         
-        pac, atl = set(), set()
-        
-        for i in range(M):
-            dfs(i,0,nums[i][0],pac)
-            dfs(i,N-1,nums[i][N-1],atl)
+            for i in range(M):
+                bfs(i,0,nums[i][0],pac)
+                bfs(i,N-1,nums[i][N-1],atl)
             
-        for j in range(N):
-            dfs(0,j,nums[0][j],pac)
-            dfs(M-1,j,nums[M-1][j],atl)
-       
-        return list(pac.intersection(atl))
+            for j in range(N):
+                bfs(0,j,nums[0][j],pac)
+                bfs(M-1,j,nums[M-1][j],atl)
+                
+            return list(pac.intersection(atl))
+        
+        return solve()
