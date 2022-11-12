2857d45d2afcf78176ab59cfeb8a06602a867ae1
Siddhant Medar <sidmedar21@gmail.com>
2022-11-11 22:35:14 -0600

    Time: 305 ms (14.29%), Space: 19.1 MB (28.66%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        
+        for word in strs:
+            container = [0]*26
+            
+            for c in word:
+                container[ord(c)-ord('a')]+=1
+                
+            mp[tuple(container)].append(word)
+        
+        return [v for _, v in mp.items()]
