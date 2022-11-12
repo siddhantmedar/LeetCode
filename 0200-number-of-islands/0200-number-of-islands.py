127513eb51fa077250cce6e5360003f14b94fcc0
Siddhant Medar <sidmedar21@gmail.com>
2022-11-12 13:40:03 -0600

    Time: 856 ms (40.15%), Space: 16.2 MB (97.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,24 @@
 class Solution:
     def numIslands(self, nums: List[List[str]]) -> int:
-        def dfs(i,j):
-            if i<0 or i>=M or j<0 or j>=N or nums[i][j] == "0":
-                return
+        def bfs(i,j):
+            q = deque([(i,j)])
             
-            nums[i][j] = "0"
-            
-            for dx,dy in directions:
-                dx+=i
-                dy+=j
+            while q:
+                n = len(q)
                 
-                dfs(dx,dy)
+                for k in range(n):
+                    i,j = q.popleft()
+                    
+                    if i<0 or i>=M or j<0 or j>=N or nums[i][j] == "0":
+                            continue
+                        
+                    nums[i][j] = "0"
+                    
+                    for dx,dy in directions:
+                        dx+=i
+                        dy+=j
+                        
+                        q.append((dx,dy))
                 
         M,N = len(nums), len(nums[0])
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
@@ -20,7 +28,7 @@ class Solution:
         for i in range(M):
             for j in range(N):
                 if nums[i][j] == "1":
-                    dfs(i,j)
+                    bfs(i,j)
                     cnt+=1
                     
         return cnt
