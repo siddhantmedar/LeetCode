be007c8d34ba8aa7ac45f40e3b9219ed8aa7ef7b
Siddhant Medar <sidmedar21@gmail.com>
2022-11-13 12:45:33 -0600

    Time: 40 ms (80.05%), Space: 14 MB (12.20%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @lru_cache
         def solve(n):
             if n==0 or n==1:
                 return 1
             
-            return solve(n-1)+solve(n-2)
+            if dp[n] != -1:
+                return dp[n]
+            
+            dp[n] = solve(n-1)+solve(n-2)
+            
+            return dp[n]
+        
+        
+        dp = [-1]*(n+1)
         
         return solve(n)
         
