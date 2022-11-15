ba910f7dfb3dabf86f0e93c850fa3d4c2d241228
Siddhant Medar <sidmedar21@gmail.com>
2022-11-15 16:42:08 -0600

    Time: 637 ms (36.28%), Space: 32.5 MB (87.92%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -7,20 +7,14 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         def solve(root, mx=float("-inf")):
-            nonlocal cnt
-            
             if not root:
-                return
+                return 0
             
+            cnt = 0
             if mx<=root.val:
-                cnt+=1
+                cnt=1
                 
-            solve(root.left, max(mx, root.val))
-            solve(root.right, max(mx, root.val))
-            
-            
-        cnt = 0
+            return cnt+solve(root.left, max(mx, root.val))+solve(root.right, max(mx, root.val))
         
-        solve(root)
         
-        return cnt
+        return solve(root)
