2e15e18d0b1dacc4fcb2c34776b40b11b4d6d76e
Siddhant Medar <sidmedar21@gmail.com>
2022-11-15 00:34:53 -0600

    Time: 110 ms (48.53%), Space: 13.9 MB (49.39%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,43 +1,50 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        def process(timestamp):
+            nonlocal fresh
+            
+            changed = False
+            
+            for i in range(M):
+                for j in range(N):
+                    if grid[i][j] == timestamp:
+                        for dx, dy in directions:
+                            dx+=i
+                            dy+=j
+
+                            if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 1:
+                                grid[dx][dy] = timestamp+1
+                                changed = True
+                                fresh-=1
+            
+            return changed
+        
+        
         M,N = len(grid), len(grid[0])
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
         
         fresh = 0
         
-        q = deque()
-        
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == 1:
                     fresh+=1
-                    
-                if grid[i][j] == 2:
-                    q.append((i,j))
         
-        if fresh == 0:  return 0
+        if fresh == 0:
+            return 0
         
         time = 0
+        ts = 2
         
-        while q:
-            n = len(q)
-                
+        while True:
             time+=1
             
-            for k in range(n):
-                i,j = q.popleft()
-                
-                for dx, dy in directions:
-                    dx+=i
-                    dy+=j
-                    
-                    if 0<=dx<M and 0<=dy<N and grid[dx][dy] == 1:
-                        grid[dx][dy] = 2
-                        q.append((dx,dy))
-                        fresh-=1                   
-                        if fresh == 0:
-                            return time
+            if not process(ts):
+                break
+            
+            if fresh == 0:
+                return time
+            
+            ts+=1
         
         return -1
-                        
-                    
