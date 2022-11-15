51989db6ea608341b0ae2cb949316f3735e9d4a3
Siddhant Medar <sidmedar21@gmail.com>
2022-11-15 17:36:02 -0600

    Time: 661 ms (32.54%), Space: 19 MB (13.10%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -13,10 +13,20 @@ class Solution:
     def pickIndex(self) -> int:
         target = self.prefix[-1]*random.random()
         
-        for i,ele in enumerate(self.prefix):
-            if target < ele:
-                return i
+        low, high = 0, len(self.prefix)-1
+        result = None
         
+        while low<=high:
+            mid = (low+high)>>1
+            
+            if self.prefix[mid] > target:
+                result = mid
+                high = mid-1
+                
+            else:
+                low = mid+1
+                
+        return result
         
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
