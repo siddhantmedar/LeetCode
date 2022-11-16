6064b82f3e5d5f9030af4d51966b01b395290b93
Siddhant Medar <sidmedar21@gmail.com>
2022-11-16 12:24:58 -0600

    Time: 324 ms (75.94%), Space: 19.1 MB (71.05%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -8,32 +8,53 @@
 
 class Solution:
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
-        def dfs(root):
+        def bfs(root):
             if not root:
                 return
             
+            q = deque([root])
             mp[root] = NodeCopy(root.val)
             
-            dfs(root.left)
-            dfs(root.right)
+            while q:
+                n = len(q)
+                
+                for k in range(n):
+                    node = q.popleft()
+                    
+                    if node.left:
+                        mp[node.left] = NodeCopy(node.left.val)
+                        q.append(node.left)
+                    if node.right:
+                        mp[node.right] = NodeCopy(node.right.val)
+                        q.append(node.right)
         
         def link(root):
             if not root:
                 return None
             
-            mp[root].left = mp[root.left]
-            mp[root].right = mp[root.right]
-            mp[root].random = mp[root.random]
+            q = deque([root])
             
-            link(root.left)
-            link(root.right)
+            while q:
+                n = len(q)
+                
+                for k in range(n):
+                    node = q.popleft()
+                    
+                    mp[node].left = mp[node.left]
+                    mp[node].right = mp[node.right]
+                    mp[node].random = mp[node.random]
+            
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
             
         if not root:
             return None
         
         mp = {None:None}
         
-        dfs(root)
+        bfs(root)
         link(root)
         
         return mp[root]
