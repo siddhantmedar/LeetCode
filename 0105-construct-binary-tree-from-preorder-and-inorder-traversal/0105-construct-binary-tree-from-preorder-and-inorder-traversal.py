fbe1ff3b66ea40cb5a27485eac9fbad90e4b1563
Siddhant Medar <sidmedar21@gmail.com>
2022-11-16 22:15:25 -0600

    Time: 52 ms (99.57%), Space: 18.9 MB (88.32%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,37 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def find(ele):
-#             low, high = 0, len(inorder)-1
-            
-#             while low<=high:
-#                 mid = (low+high)>>1
-                
-#                 if inorder[mid] == ele:
-#                     print(mid)
-#                     return mid
-                
-#                 elif inorder[mid] > ele:
-#                     high = mid-1
-                    
-#                 else:
-#                     low = mid+1
-            for i,x in enumerate(inorder):
-                if x == ele:
-                    return i
-            
-        
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:       
         def solve(ps,pe,iss,ie):
             if ps>pe or iss>ie:
                 return None
             
             root = TreeNode(preorder[ps])
-            idx = find(preorder[ps])
+            idx = find[preorder[ps]]
             
             root.left = solve(ps+1,ps+(idx-iss),iss,idx-1)
             root.right = solve(ps+(idx-iss)+1,pe,idx+1,ie)
             
             return root
+        
+        find = {ele:i for i,ele in enumerate(inorder)}
         
         return solve(0,len(preorder)-1,0,len(inorder)-1)
