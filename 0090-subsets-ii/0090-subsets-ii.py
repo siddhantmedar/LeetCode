c7a9a35c6b4a61056f8d962d8392672aa4c9438b
Siddhant Medar <sidmedar21@gmail.com>
2022-11-17 17:54:13 -0600

    Time: 53 ms (79.14%), Space: 14.1 MB (93.35%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -2,26 +2,24 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         
-        def solve(idx):
-            nonlocal path
+        def solve(idx,path):
+            nonlocal result
             
             if idx == len(nums):
                 result.append(path[::])
                 return
             
-            path.append(nums[idx])
-            solve(idx+1)
-            path.pop()
+            solve(idx+1,path+[nums[idx]])
             
             while idx+1<len(nums) and nums[idx] == nums[idx+1]:
                 idx+=1
                 
-            solve(idx+1)
+            solve(idx+1,path)
             
             
         result = []
         path = []
         
-        solve(0)
+        solve(0,[])
 
         return result
