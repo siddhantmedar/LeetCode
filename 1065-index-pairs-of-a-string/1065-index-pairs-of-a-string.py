48ccbaf542b30f91892e55537147240b602d21eb
Siddhant Medar <sidmedar21@gmail.com>
2022-11-17 00:34:30 -0600

    Time: 102 ms (24.92%), Space: 14.1 MB (38.83%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -1,38 +1,36 @@
-class Trie:
+class TrieNode:
     def __init__(self):
-        self.children = dict()
+        self.children = {}
         self.end = False
         
     @staticmethod
     def insertWords(words):
-        root = Trie()
+        root = TrieNode()
         
         for word in words:
             node = root
-            
             for c in word:
                 if c not in node.children:
-                    node.children[c]=Trie()
+                    node.children[c] = TrieNode()
                     
                 node = node.children[c]
                 
             node.end = True
             
         return root
-        
-        
+    
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
-        root = Trie().insertWords(words)
+        root = TrieNode.insertWords(words)
         
         result = []
         
         for i in range(len(text)):
             node = root
-            for j in range(i, len(text)):
+            for j in range(i,len(text)):
                 if text[j] not in node.children:
                     break
-                    
+                
                 node = node.children[text[j]]
                 
                 if node.end:
