6a4357f10578cf5965f0de0f71280194e6a2dc5d
Siddhant Medar <sidmedar21@gmail.com>
2022-11-18 00:00:52 -0600

    Time: 61 ms (44.06%), Space: 13.9 MB (66.45%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def solve(idx):
+            if idx>=len(nums):
+                return 0
+            
+            if dp[idx] != -1:
+                return dp[idx]
+            
+            exclude = solve(idx+1)
+            include = solve(idx+2)+nums[idx]
+            
+            dp[idx] = max(include, exclude)
+            
+            return dp[idx]
+        
+        
+        dp = [-1 for i in range(len(nums)+1)]
+        
+        return solve(0)
