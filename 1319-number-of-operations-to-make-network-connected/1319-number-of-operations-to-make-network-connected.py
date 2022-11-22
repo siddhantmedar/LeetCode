de78831e8473158ac9a008393981875d83bc490b
Siddhant Medar <sidmedar21@gmail.com>
2022-11-21 22:26:48 -0600

    Time: 1377 ms (27.53%), Space: 34.2 MB (74.59%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,24 +1,24 @@
 class Solution:
     def makeConnected(self, n: int, edges: List[List[int]]) -> int:
-        if len(edges) < n-1:
-            return -1
-        
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
+                
             return parent[x]
         
         def union(x,y):
-            setX = find(x)
-            setY = find(y)
+            setX, setY = find(x), find(y)
             
             if setX != setY:
                 parent[setY] = setX
-                
-            
-        parent = {i:i for i in range(n)}
+        
+        
+        if len(edges) < n-1:
+            return -1
+        
+        parent = {x:x for x in range(n)}
         
         for u,v in edges:
             union(u,v)
-            
-        return sum([1 for k,v in parent.items() if k == v])-1
+        
+        return sum([1 for k,v in parent.items() if k==v])-1
