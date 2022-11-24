4f595f04a3ba9e194879ec6ca504bd4636d755f3
Siddhant Medar <sidmedar21@gmail.com>
2022-11-23 21:43:51 -0600

    Time: 1455 ms (17.36%), Space: 43.3 MB (35.00%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,24 +1,28 @@
 class Solution:
     def makeConnected(self, n: int, edges: List[List[int]]) -> int:
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-                
-            return parent[x]
-        
-        def union(x,y):
-            setX, setY = find(x), find(y)
+        def dfs(node):
+            visited.add(node)
             
-            if setX != setY:
-                parent[setY] = setX
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
         
         
         if len(edges) < n-1:
             return -1
         
-        parent = {x:x for x in range(n)}
+        graph = defaultdict(set)
         
         for u,v in edges:
-            union(u,v)
+            graph[u].add(v)
+            graph[v].add(u)
+            
+        visited = set()
+        cnt = 0
         
-        return sum([1 for k,v in parent.items() if k==v])-1
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                cnt+=1
+                
+        return cnt-1
