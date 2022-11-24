e36d817e171050de56ef80ddef813c796de56a23
Siddhant Medar <sidmedar21@gmail.com>
2022-11-24 13:16:05 -0600

    Time: 105 ms (44.10%), Space: 14.4 MB (85.08%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -6,27 +6,30 @@
 #         self.right = right
 class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        q = deque([root])
-        
-        while q:
-            n = len(q)
-            tmp = []
-            for k in range(n):
-                node = q.popleft()
-                tmp.append(node)
-                
-                if node.left:
-                    q.append(node.left)
+        def bfs(root):
+            q = deque([root])
+
+            while q:
+                n = len(q)
+                tmp = []
+                for _ in range(n):
+                    node = q.popleft()
+                    tmp.append(node)
                     
-                if node.right:
-                    q.append(node.right)
+                    if node.left:
+                        q.append(node.left)
+
+                    if node.right:
+                        q.append(node.right)
+            
+            return tmp
         
         
         def dfs(root):
             if not root:
                 return root
             
-            if root in tmp:
+            if root in set(nodes):
                 return root
             
             left = dfs(root.left)
@@ -36,5 +39,7 @@ class Solution:
                 return root
             
             return left or right
+        
+        nodes = bfs(root)
         
         return dfs(root)
