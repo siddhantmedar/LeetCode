2901f5f4cc73c9fbd38a53fdaadab018fd54d7d0
Siddhant Medar <sidmedar21@gmail.com>
2022-11-23 22:42:08 -0600

    Time: 181 ms (31.87%), Space: 14.2 MB (40.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,5 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def bfs(i,j,pre,c):
+            q = deque([(i,j)])
+            
+            while q:
+                n = len(q)
+                
+                for _ in range(n):
+                    i,j = q.popleft()
+                    
+                    image[i][j] = c
+                    
+                    for dx,dy in directions:
+                        dx+=i
+                        dy+=j
+                        
+                        if 0<=dx<M and 0<=dy<N and image[dx][dy] == pre and image[dx][dy] != c:
+                            q.append((dx,dy))
+                    
+        
         def dfs(i,j,pre,c):
             if i<0 or i>=M or j<0 or j>=N or image[i][j] != pre or image[i][j] == c:
                 return
@@ -16,6 +35,6 @@ class Solution:
         M,N = len(image),len(image[0])
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
         
-        dfs(sr,sc,image[sr][sc],color)
+        bfs(sr,sc,image[sr][sc],color)
         
         return image
