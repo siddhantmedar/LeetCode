2bb40b7cddb68d2ca978935e4755d25a2e43aef7
Siddhant Medar <sidmedar21@gmail.com>
2022-11-24 00:29:17 -0600

    Time: 1481 ms (53.59%), Space: 43.8 MB (5.40%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        mp = defaultdict(list)
+        mp = defaultdict(set)
         
         for i,ele in enumerate(nums):
             if ele in mp:
                 for idx in mp[ele]:
                     if i-idx <= k:
                         return True
-                    
-            mp[ele].append(i)
+            
+            mp[ele].add(i)
         
         return False
