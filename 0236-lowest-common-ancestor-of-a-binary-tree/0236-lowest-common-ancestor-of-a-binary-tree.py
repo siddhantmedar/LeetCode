cdfe4c366c9d69158bd4764c8f9767df96649b06
Siddhant Medar <sidmedar21@gmail.com>
2022-11-24 13:10:57 -0600

    Time: 205 ms (12.67%), Space: 28.6 MB (7.09%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,20 +7,42 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(root):
+        def dfs(root,target,lst):
             if not root:
-                return
+                return False
             
-            if root == p or root == q:
-                return root
+            lst.append(root.val)
+            mp[root.val] = root
+            if root == target:
+                return True
             
-            left = dfs(root.left)
-            right = dfs(root.right)
+            if dfs(root.left,target,lst) or dfs(root.right,target,lst):
+                return True
             
-            if left and right:
-                return root
+            lst.pop()
             
-            return left or right
+            return False
         
         
-        return dfs(root)
+        lst1,lst2 = [],[]
+        mp = {}
+        
+        dfs(root,p,lst1)
+        dfs(root,q,lst2)
+        
+        result = None
+        
+        l1,l2 = set(lst1), set(lst2)
+        result = None
+        
+        if len(lst1) < len(lst2):
+            for i in range(len(lst1)):
+                if lst1[i] in l2:
+                    result = lst1[i]
+            
+        else:
+            for i in range(len(lst2)):
+                if lst2[i] in l1:
+                    result = lst2[i]
+            
+        return mp[result]
