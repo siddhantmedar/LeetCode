362cf1af5136470a2e7a8ec949c7b5cdcd82953b
Siddhant Medar <sidmedar21@gmail.com>
2022-11-24 00:04:27 -0600

    Time: 260 ms (21.28%), Space: 17.1 MB (25.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,5 +1,36 @@
 class Solution:
     def canFinish(self, n: int, edges: List[List[int]]) -> bool:
+        def bfs():
+            graph = defaultdict(set)
+            indegree = {node:0 for node in range(n)}
+            
+            for u,v in edges:
+                graph[v].add(u)
+                indegree[u]=1+indegree.get(u,0)
+            
+            cnt = 0
+            result = []
+            
+            q = deque([node for node,cnt in indegree.items() if cnt == 0])
+            
+            while q:
+                k = len(q)
+                
+                for _ in range(k):
+                    node = q.popleft()
+                    result.append(node)
+                    
+                    cnt+=1
+                    
+                    for nei in graph[node]:
+                        indegree[nei]-=1
+                        
+                        if indegree[nei] == 0:
+                            q.append(nei)
+                            
+            return cnt if cnt == len(indegree) else []
+            
+        
         def dfs(node):
             visited.add(node)
             rec.add(node)
@@ -27,11 +58,13 @@ class Solution:
         rec = set()
         st = deque()
         
-        for i in range(n):
-            if i not in visited:
-                if not dfs(i):
-                    return False
+#         for i in range(n):
+#             if i not in visited:
+#                 if not dfs(i):
+#                     return False
                 
-        # print(st)
+#         # print(st)
         
-        return True
+#         return True
+
+        return bfs()
