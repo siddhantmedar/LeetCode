dacbe1d56aa11ede4381afbdc51a374c19ff6132
Siddhant Medar <sidmedar21@gmail.com>
2022-11-24 00:51:12 -0600

    Time: 117 ms (65.20%), Space: 14.4 MB (81.92%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -6,29 +6,29 @@ class Solution:
         n = points[1][1]-points[0][1]
         d = points[1][0]-points[0][0]
         
-        if n == 0:
-            slope = 0
+        if n and d:
+            ref = n/d
             
         elif d == 0:
-            slope = float("inf")
+            ref = float("inf")
             
-        else:
-            slope = n/d
-        
-        for i in range(1, len(points)-1):
+        elif n == 0:
+            ref = 0
+            
+        for i in range(len(points)-1):
             n = points[i+1][1]-points[i][1]
             d = points[i+1][0]-points[i][0]
-            
-            if n == 0:
-                tmp = 0
-            
-            elif d == 0:
-                tmp = float("inf")
 
-            else:
-                tmp = n/d
-            
-            if tmp != slope:
+            if n and d:
+                slope = n/d
+
+            elif d == 0:
+                slope = float("inf")
+
+            elif n == 0:
+                slope = 0
+                
+            if slope != ref:
                 return False
             
         return True
