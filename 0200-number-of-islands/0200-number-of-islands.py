dbaea77f03d338c80b94758295255fab35b40e2e
Siddhant Medar <sidmedar21@gmail.com>
2022-11-23 21:33:49 -0600

    Time: 621 ms (66.19%), Space: 35 MB (5.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(i,j):
-            q = deque([(i,j)])
-            
-            while q:
-                n = len(q)
+        def find(x,y):
+            if parent[(x,y)] != (x,y):
+                parent[(x,y)] = find(parent[(x,y)][0], parent[(x,y)][1])
                 
-                for _ in range(n):
-                    i,j = q.popleft()
-                    
-                    if i<0 or i>=M or j<0 or j>=N or grid[i][j] == "0":
-                        continue
-                    
-                    grid[i][j] = "0"
-                    
-                    for dx,dy in directions:
-                        dx+=i
-                        dy+=j
-                        
-                        q.append((dx,dy))
-                    
+            return parent[(x,y)]
+        
+        
+        def union(x,y,a,b):
+            setX = find(x,y)
+            setY = find(a,b)
+            
+            if setX != setY:
+                if size[setX] >= size[setY]:
+                    parent[setY] = setX
+                
+                elif size[setX] < size[setY]:
+                    parent[setX] = setY
+        
             
         M,N = len(grid),len(grid[0])
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
-        
+        parent = {(i,j):(i,j) for i in range(M) for j in range(N) if grid[i][j] == "1"}
+        size = {(i,j):1 for i in range(M) for j in range(N) if grid[i][j] == "1"}
         result = 0
         
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == "1":
-                    result+=1
-                    bfs(i,j)
+                    for dx,dy in directions:
+                        dx+=i
+                        dy+=j
+                        
+                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == "1":
+                            union(i,j,dx,dy)
                     
-        return result
+        return sum([1 for k,v in parent.items() if k==v])
