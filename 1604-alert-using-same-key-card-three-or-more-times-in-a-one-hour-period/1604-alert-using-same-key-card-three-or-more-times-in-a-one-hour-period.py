6c39362d8865428747a23f907a99af949fe3a7cf
Siddhant Medar <sidmedar21@gmail.com>
2022-11-25 00:07:38 -0600

    Time: 2333 ms (17.54%), Space: 38.7 MB (37.50%) - LeetHub

---
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -0,0 +1,37 @@
+class Solution:
+    def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
+        def convert(time):
+            hr,mn = time[:2],time[3:]
+            
+            return int(hr)*60+int(mn)
+        
+        def check(lst):
+            q = deque()
+            for time in lst:
+                while q and time-q[0] > 60:
+                    q.popleft()
+                
+                q.append(time)
+                if len(q) == 3:
+                    return True
+                
+            return False
+            
+        
+        keyTime = [convert(t) for t in keyTime]
+        
+        mp = defaultdict(list)
+        
+        for i in range(len(keyName)):
+            mp[keyName[i]].append(keyTime[i])
+            
+        result = []
+        
+        for k,v in mp.items():
+            v.sort()
+            if check(v):
+                result.append(k)
+        
+        result.sort()
+        
+        return result
