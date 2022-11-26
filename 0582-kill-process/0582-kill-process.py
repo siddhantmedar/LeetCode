59546bb7a84c0796be8523c4adda8d98ebc438c9
Siddhant Medar <sidmedar21@gmail.com>
2022-11-25 18:04:53 -0600

    Time: 1323 ms (13.37%), Space: 35.4 MB (5.08%) - LeetHub

---
+++ 0582-kill-process/0582-kill-process.py
@@ -0,0 +1,23 @@
+class Solution:
+    def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
+        def buildPath(mp,node):
+            if not mp[node]:
+                return set([node])
+            ans = set()
+            ans.add(node)
+            
+            for ele in mp[node]:
+                ans.update(buildPath(mp,ele))
+            
+            return ans
+        
+        
+        mp = defaultdict(set)
+        
+        for i in range(len(pid)):
+            if ppid[i] == 0:
+                continue
+            
+            mp[ppid[i]].add(pid[i])
+            
+        return list(buildPath(mp,kill))
