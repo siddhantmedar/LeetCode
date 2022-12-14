f134e570418524e7363b1a0d4d8636f5279c5328
Siddhant Medar <sidmedar21@gmail.com>
2022-12-14 12:30:24 -0600

    Time: 113 ms (87.71%), Space: 18.7 MB (71.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,27 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], K: int) -> List[int]:
-        mp = {}
+        mp = dict()
         
         for ele in nums:
-            mp[ele] = 1+mp.get(ele,0)
+            mp[ele]=1+mp.get(ele,0)
             
-        size = max([v for k,v in mp.items()])
+        print(mp)
         
-        container = [set() for _ in range(size+1)]
+        mp = dict(sorted(mp.items(),key=lambda x:-x[1]))
         
-        for k,v in mp.items():
-            container[v].add(k)
-            
-        result = []
+        print(mp)
         
-        for i in range(len(container)-1,-1,-1):
-            box = container[i]
-            
-            if box:
-                for ele in box:
-                    if len(result) == K:
-                        break
-                    
-                    result.append(ele)
-                    
-        return result
+        return [k for k,v in mp.items()][:K]
