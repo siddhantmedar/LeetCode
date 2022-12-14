70d3d6a46de343b3a81f9008f34571c2ef9dded5
Siddhant Medar <sidmedar21@gmail.com>
2022-12-14 13:59:48 -0600

    Time: 360 ms (91.42%), Space: 15.6 MB (73.58%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,22 +1,22 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
         count = [0]*n
-        mat = [[0]*n for _ in range(n)]
+        adj = [[0]*n for _ in range(n)]
         
         for u,v in roads:
-            if mat[u][v] or mat[v][u]:
+            if adj[u][v] or adj[v][u]:
                 continue
-            
+                
             count[u]+=1
             count[v]+=1
             
-            mat[u][v] = 1
-            mat[v][u] = 1
+            adj[u][v]=1
+            adj[v][u]=1
             
-        res = 0
+        result = 0
         
-        for i in range(n):
-            for j in range(i+1,n):
-                res = max(res,count[i]+count[j]-mat[i][j])
-                
-        return res
+        for u in range(n):
+            for v in range(u+1,n):
+                result = max(result,count[u]+count[v]-adj[u][v])
+        
+        return result
