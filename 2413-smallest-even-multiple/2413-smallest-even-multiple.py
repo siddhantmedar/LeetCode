26dfe1abfa572e0371516eae7448f8b39300b6c7
Siddhant Medar <sidmedar21@gmail.com>
2022-12-22 00:02:37 -0600

    Time: 37 ms (77.57%), Space: 13.9 MB (6.84%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        i=1
+        
+        while True:
+            if i%2==0 and i%n==0:
+                return i
+            i+=1
