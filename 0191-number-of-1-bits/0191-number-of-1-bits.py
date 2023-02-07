19b40aff8ff2961507a8153563174e6fe214ae22
Siddhant Medar <sidmedar21@gmail.com>
2023-02-06 21:47:29 -0600

    Time: 39 ms (38.87%), Space: 13.8 MB (43.78%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,7 +3,8 @@ class Solution:
         cnt = 0
         
         while n:
-            cnt+=n&1
-            n>>=1
+            cnt+=1
+            
+            n = n&(n-1)
             
         return cnt
