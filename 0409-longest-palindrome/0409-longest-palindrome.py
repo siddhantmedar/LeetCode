bb2e576165596c4813e98d66d5fbc3cfee3a0baf
Siddhant Medar <sidmedar21@gmail.com>
2023-02-20 18:49:52 -0600

    Time: 33 ms (74.94%), Space: 13.9 MB (14.78%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,19 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         mp = {}
-        
         for c in s:
-            mp[c] = 1+mp.get(c,0)
+            mp[c]=1+mp.get(c,0)
             
-        res = 0
         odd = False
+        res=0
         
         for k,v in mp.items():
-            if v%2:
-                res+=(v-1)
-                odd = True
-                
-            else:
+            if v%2==0:
                 res+=v
-                
+            else:
+                res+=(v-1)
+                if not odd:
+                    odd = True
+                    
         return res if not odd else res+1
