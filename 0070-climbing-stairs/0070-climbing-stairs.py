a8ce68a6d213f6dbb08585bb0acdfa0f500d66e4
Siddhant Medar <sidmedar21@gmail.com>
2023-02-20 19:04:30 -0600

    Time: 36 ms (37.27%), Space: 13.9 MB (48.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -11,6 +11,15 @@ class Solution:
             return dp[n]
         
         
-        dp = [-1]*(n+1)
+        # dp = [-1]*(n+1)
         
-        return solve(n)
+        # return solve(n)
+        
+        a,b=1,1
+        
+        for _ in range(n-1):
+            c = a+b
+            a=b
+            b=c
+        
+        return b
