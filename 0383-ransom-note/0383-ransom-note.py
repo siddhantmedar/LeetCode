c82671de1e8c3abbe1cabd89a29ba4854badc205
Siddhant Medar <sidmedar21@gmail.com>
2023-02-20 18:38:22 -0600

    Time: 69 ms (47.18%), Space: 14.1 MB (46.38%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -4,7 +4,7 @@ class Solution:
         
         for c in magazine:
             mp[c] = 1+mp.get(c,0)
-        
+            
         for c in ransomNote:
             if c not in mp:
                 return False
@@ -12,7 +12,7 @@ class Solution:
             else:
                 mp[c]-=1
                 
-                if mp[c] == 0:
+                if mp[c]==0:
                     del mp[c]
                     
         return True
