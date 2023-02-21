b82c2d855236d95134da31c21f9703211780189b
Siddhant Medar <sidmedar21@gmail.com>
2023-02-20 18:11:24 -0600

    Time: 81 ms (55.51%), Space: 14.1 MB (61.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,40 +1,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def bfs(i,j,pre,c):
-            q = deque([(i,j)])
+        def dfs(r,c,oldc,newc):
+            if r<0 or r>=M or c<0 or c>=N or image[r][c]==newc or image[r][c] != oldc:
+                return
+            image[r][c] = newc
             
-            while q:
-                n = len(q)
-                
-                for _ in range(n):
-                    i,j = q.popleft()
-                    
-                    image[i][j] = c
-                    
-                    for dx,dy in directions:
-                        dx+=i
-                        dy+=j
-                        
-                        if 0<=dx<M and 0<=dy<N and image[dx][dy] == pre and image[dx][dy] != c:
-                            q.append((dx,dy))
-                    
+            for dx,dy in directions:
+                dx+=r
+                dy+=c
+                dfs(dx,dy,oldc,newc)
         
-#         def dfs(i,j,pre,c):
-#             if i<0 or i>=M or j<0 or j>=N or image[i][j] != pre or image[i][j] == c:
-#                 return
-            
-#             image[i][j] = c
-            
-#             for dx,dy in directions:
-#                 dx+=i
-#                 dy+=j
-                
-#                 dfs(dx,dy,pre,c)
-            
         
-        M,N = len(image),len(image[0])
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        M,N = len(image), len(image[0])
         
-        bfs(sr,sc,image[sr][sc],color)
+        directions = [(0,-1),(0,1),(-1,0),(1,0)]
+        
+        dfs(sr,sc,image[sr][sc],color)
         
         return image
+            
