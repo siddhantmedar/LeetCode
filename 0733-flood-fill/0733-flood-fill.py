a35efd7eef58cc3fd538450d50ba54ce7cce1f6c
Siddhant Medar <sidmedar21@gmail.com>
2023-02-20 18:14:59 -0600

    Time: 69 ms (97.28%), Space: 14.1 MB (33.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -10,12 +10,31 @@ class Solution:
                 dy+=c
                 dfs(dx,dy,oldc,newc)
         
+        def bfs(r,c,oldc,newc):
+            q = deque([(r,c)])
+            
+            while q:
+                n = len(q)
+                
+                for _ in range(n):
+                    r,c = q.popleft()
+                        
+                    image[r][c] = newc
+            
+                    for dx,dy in directions:
+                        dx+=r
+                        dy+=c
+                        
+                        if not (dx<0 or dx>=M or dx<0 or dy>=N or image[dx][dy]==newc or\
+                        image[dx][dy] != oldc):
+                            dfs(dx,dy,oldc,newc)
+        
         
         M,N = len(image), len(image[0])
         
         directions = [(0,-1),(0,1),(-1,0),(1,0)]
         
-        dfs(sr,sc,image[sr][sc],color)
+        bfs(sr,sc,image[sr][sc],color)
         
         return image
             
