b4516702ef3c9bf0b19a398b91bd9e59f07278c0
Siddhant Medar <sidmedar21@gmail.com>
2023-02-22 00:19:40 -0600

    Time: 105 ms (12.22%), Space: 13.7 MB (99.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,17 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        if x<0:
             return False
         
+        rev = 0
+        
         def solve(n):
-            nonlocal res
-            
             if n>0:
-                res = res*10+(n%10)
+                nonlocal rev
+                rev = rev*10+(n%10)
                 solve(n//10)
-                
-        res = 0
         
         solve(x)
         
-        return x == res
+        return rev==x
