ae41bc1a3aa7cb101913ff78d8638ab052539e3a
Siddhant Medar <sidmedar21@gmail.com>
2023-04-18 23:29:51 -0500

    Time: 324 ms (5.26%), Space: 14.5 MB (30.29%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], cap: int) -> bool:
+        lst = [0 for _ in range(1001)]
+        pre = None
+        trips.sort(key=lambda x:(x[1],x[2]))
+        for p,_,_ in trips:
+            if p>cap:
+                return False
+        
+        for p,t,f in trips:
+            for i in range(t,f):
+                lst[i]+=p
+                if lst[i] > cap:
+                    return False
+        
+        return True
