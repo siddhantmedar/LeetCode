0a5dbb6bcafab966b490afd774409f2ed330f4eb
Siddhant Medar <sidmedar21@gmail.com>
2023-06-16 01:08:32 -0500

    Time: 129 ms (18.05%), Space: 29.8 MB (5.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,7 +4,25 @@ class Solution:
         
         for ele in nums:
             mp[ele] = 1 + mp.get(ele,0)
-            
-        res = sorted(mp.items(),key=lambda x:-x[1])
         
-        return [x[0] for x in res][:k]
+        container = [set() for _ in range(len(nums)+1)]
+        
+        for key,val in mp.items():
+            container[val].add(key)
+        
+        i = len(container)-1
+        
+        res = []
+        
+        while i>=0:
+            if k==0:
+                break
+            
+            if container[i]:
+                for ele in container[i]:
+                    if k:
+                        k-=1
+                        res.append(ele)
+            i-=1
+        
+        return res
