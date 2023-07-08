c6fa8f6d4cbd391260a46633c6b872f6a869b31d
Siddhant Medar <sidmedar21@gmail.com>
2023-07-08 15:35:35 -0500

    Time: 131 ms (27.52%), Space: 21.6 MB (32.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,11 +2,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mp = defaultdict(list)
         
-        for ele in strs:
-            hash = [0]*26
-            for c in ele:
-                hash[ord(c)-ord('a')]+=1 
-                
-            mp[tuple(hash)].append(ele)
+        for word in strs:
+            key = [0]*26
             
-        return [v for k,v in mp.items()]
+            for c in word:
+                key[ord(c)-ord('a')]+=1
+                
+            mp[tuple(key)].append(word)
+            
+            
+        return [word_list for _, word_list in mp.items()]
