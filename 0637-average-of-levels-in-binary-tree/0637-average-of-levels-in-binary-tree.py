0c1618f6bf581c1e29773f51cef7217aa280349b
Siddhant Medar <sidmedar21@gmail.com>
2023-07-08 16:38:26 -0500

    Time: 77 ms (10.68%), Space: 19 MB (39.82%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -8,6 +8,32 @@ class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         mp = defaultdict(list)
         
+        def bfs(root):
+            if not root:
+                return []
+            
+            res = []
+            
+            q = deque([root])
+            
+            while q:
+                n = len(q)
+                tmp = []
+                for _ in range(n):
+                    node = q.popleft()
+                    tmp.append(node.val)
+
+                    if node.left:
+                        q.append(node.left)
+
+                    if node.right:
+                        q.append(node.right)
+                    
+                res.append(sum(tmp)/len(tmp))
+            
+            return res
+        
+        
         def dfs(root, l):
             if not root:
                 return
@@ -17,6 +43,6 @@ class Solution:
             dfs(root.left, l+1)
             dfs(root.right, l+1)
             
-        dfs(root,1)
+        return bfs(root)
         
-        return [sum(v)/len(v) for _,v in mp.items()]
+        # return [sum(v)/len(v) for _,v in mp.items()]
