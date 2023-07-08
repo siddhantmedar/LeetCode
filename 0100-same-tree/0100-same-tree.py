a3c45a993eb491507b2d6fd1a431f1e612115b21
Siddhant Medar <sidmedar21@gmail.com>
2023-07-08 17:07:50 -0500

    Time: 48 ms (49.98%), Space: 16.4 MB (56.89%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,6 +6,31 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def bfs(r1, r2):            
+            q = deque([(r1,r2)])
+            
+            while q:
+                n=len(q)
+                
+                for _ in range(n):
+                    r1, r2 = q.popleft()
+                    
+
+                    if (r1 and not r2) or (not r1 and r2):
+                        return False
+                    
+                    if not r1 and not r2:
+                        continue
+                    
+                    if not (r1.val==r2.val):
+                        return False
+                    
+                    q.append((r1.left,r2.left))
+                    q.append((r1.right,r2.right))
+                    
+            return True
+        
+        
         def dfs(r1, r2):
             if not r1 and not r2:
                 return True
@@ -15,4 +40,4 @@ class Solution:
             
             return r1.val==r2.val and dfs(r1.left,r2.left) and dfs(r1.right,r2.right)
         
-        return dfs(p,q)
+        return bfs(p,q)
