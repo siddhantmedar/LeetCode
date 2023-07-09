ef90980504037e08cf9f829738eb349d969d6d48
Siddhant Medar <sidmedar21@gmail.com>
2023-07-09 02:07:49 -0500

    Time: 615 ms (5.10%), Space: 37.4 MB (5.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,39 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i,j):
-            if i<0 or i>=M or j<0 or j>=N or grid[i][j] == "0":
-                return
-            
-            grid[i][j] = "0"
-            
-            for dx,dy in dir:
-                dx+=i 
-                dy+=j
+        def find(x,y):
+            if parent[(x,y)] != (x,y):
+                parent[(x,y)] = find(parent[(x,y)][0], parent[(x,y)][1])
                 
-                dfs(dx,dy)
+            return parent[(x,y)]
+        
+        def union(x,y,x_,y_):
+            setX = find(x,y)
+            setY = find(x_,y_)
+            
+            if setX != setY:
+                if size[setX] >= size[setY]:
+                    size[setX]+=size[setY]
+                    parent[setY] = setX
+                    
+                else:
+                    size[setY]+=size[setX]
+                    parent[setX] = setY
                 
+        
         M,N = len(grid), len(grid[0])
-        cnt = 0
+        parent = {(x,y):(x,y) for x in range(M) for y in range(N) if grid[x][y] == "1"}
+        size = {(x,y):1 for x in range(M) for y in range(N) if grid[x][y] == "1"}
+        
         dir = [(-1,0),(1,0),(0,1),(0,-1)]
+        
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == "1":
-                    cnt+=1
-                    dfs(i,j)
+                    for dx,dy in dir:
+                        dx+=i 
+                        dy+=j
+                        
+                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == "1":
+                            union(i,j,dx,dy)
         
-        return cnt
+        return (sum([1 for k,v in parent.items() if k==v]))
