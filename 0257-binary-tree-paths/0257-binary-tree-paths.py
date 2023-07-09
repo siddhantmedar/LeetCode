8583603318ad8d60b03969a7554ad7e9ea8884b8
Siddhant Medar <sidmedar21@gmail.com>
2023-07-08 23:27:58 -0500

    Time: 49 ms (56.40%), Space: 16.1 MB (99.14%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,28 +6,54 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        def dfs(root):
-            nonlocal res, path
+        def bfs(root):
+            q = deque([(root,[])])
+            res = []
             
-            if not root:
-                return 
-            
-            path.append(str(root.val))
-            
-            if not root.left and not root.right:
-                res.append("->".join(path))
-                path.pop()
-                return
-            
-            dfs(root.left)
-            dfs(root.right)
-            
-            path.pop()
+            while q:
+                n = len(q)
+                
+                for _ in range(n):
+                    node,path = q.popleft()
+                    
+                    if not node:
+                        continue
+                        
+                    path.append(str(node.val))
+
+                    if not node.left and not node.right:
+                        res.append("->".join(path))
+                    
+                    if node.left:
+                        q.append((node.left,path[::]))
+                    if node.right:
+                        q.append((node.right,path[::]))
+
+            return res
         
         
-        res = []
-        path = []
+        # def dfs(root):
+#             nonlocal res, path
+            
+#             if not root:
+#                 return 
+            
+#             path.append(str(root.val))
+            
+#             if not root.left and not root.right:
+#                 res.append("->".join(path))
+#                 path.pop()
+#                 return
+            
+#             dfs(root.left)
+#             dfs(root.right)
+            
+#             path.pop()
         
-        dfs(root)
         
-        return res
+        # res = []
+        # path = []
+        
+        return bfs(root)
+        
+        # return res
