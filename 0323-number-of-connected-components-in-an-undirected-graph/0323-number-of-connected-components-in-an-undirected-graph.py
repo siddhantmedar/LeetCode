9299677cdcb00a440f0fe575d0f30473e126c425
Siddhant Medar <sidmedar21@gmail.com>
2023-07-09 02:18:39 -0500

    Time: 115 ms (71.83%), Space: 17.8 MB (62.21%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -3,19 +3,23 @@ class Solution:
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
-
+                
             return parent[x]
-
+        
         def union(x,y):
             setX = find(x)
             setY = find(y)
             
             if setX != setY:
                 parent[setY] = setX
-                
-        parent = {i:i for i in range(n)}
         
-        for u,v in edges:
+        
+        parent = {x:x for x in range(n)}
+        size = {x:1 for x in range(n)}
+        
+        dir = [(-1,0),(1,0),(0,1),(0,-1)]
+        
+        for u, v in edges:
             union(u,v)
-            
-        return sum([1 for k,v in parent.items() if k==v])
+        
+        return (sum([1 for k,v in parent.items() if k==v]))
