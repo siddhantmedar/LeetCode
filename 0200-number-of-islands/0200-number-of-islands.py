b9b8347a30d847431a5a574ec72f73642aca43e8
Siddhant Medar <sidmedar21@gmail.com>
2023-07-09 01:56:53 -0500

    Time: 321 ms (80.31%), Space: 18.9 MB (78.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,39 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def find(x,y):
-            if parent[(x,y)] != (x,y):
-                parent[(x,y)] = find(parent[(x,y)][0],parent[(x,y)][1])
-                
-            return parent[(x,y)]
-        
-        def union(i,j,x,y):
-            setX = find(i,j)
-            setY = find(x,y)
+        def dfs(i,j):
+            if i<0 or i>=M or j<0 or j>=N or grid[i][j] == "0":
+                return
             
-            if setX != setY:
-                if size[setX] >= size[setY]:
-                    parent[setY] = setX
-                    size[setX]+=size[setY]
-                    
-                else:
-                    parent[setX] = setY
-                    size[setY]+=size[setX]
-                    
-        
-        M,N = len(grid),len(grid[0])
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
-        
-        parent = {(i,j):(i,j) for i in range(M) for j in range(N) if grid[i][j] == "1"}
-        size = {(i,j):1 for i in range(M) for j in range(N) if grid[i][j] == "1"}
-        
+            grid[i][j] = "0"
+            
+            for dx,dy in dir:
+                dx+=i 
+                dy+=j
+                
+                dfs(dx,dy)
+                
+        M,N = len(grid), len(grid[0])
+        cnt = 0
+        dir = [(-1,0),(1,0),(0,1),(0,-1)]
         for i in range(M):
             for j in range(N):
                 if grid[i][j] == "1":
-                    for dx,dy in directions:
-                        dx+=i
-                        dy+=j
-                        
-                        if 0<=dx<M and 0<=dy<N and grid[dx][dy] == "1":
-                            union(i,j,dx,dy)
+                    cnt+=1
+                    dfs(i,j)
         
-        return sum([1 for k,v in parent.items() if k==v])
+        return cnt
