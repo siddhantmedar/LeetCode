b6aa7f2f2cabb1dd257145b88cb473c6710405c4
Siddhant Medar <sidmedar21@gmail.com>
2023-07-09 14:05:39 -0500

    Time: 121 ms (79.34%), Space: 17.6 MB (13.23%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -3,31 +3,27 @@ class Solution:
         if len(p) > len(s):
             return []
         
-        k = len(p)
-        result = []
-        
+        res = []
+        k=len(p)
         ref = {}
-        
         for c in p:
-            ref[c] = 1+ref.get(c,0)
-        
-        mp = {}
-        
-        for c in s[:k]:
-            mp[c] = 1+mp.get(c,0)
+            ref[c]=1+ref.get(c,0)
             
-        if mp == ref:
-            result.append(0)
+        mp = {}
+        for i in range(k):
+            mp[s[i]]=1+mp.get(s[i],0)
+            
+        if ref == mp:
+            res.append(0)
             
         for i in range(k,len(s)):
             mp[s[i-k]]-=1
-            
-            if mp[s[i-k]] == 0:
+            if mp[s[i-k]]==0:
                 del mp[s[i-k]]
                 
             mp[s[i]] = 1+mp.get(s[i],0)
             
-            if ref == mp:
-                result.append(i-k+1)
+            if mp==ref:
+                res.append(i-k+1)
                 
-        return result
+        return res
