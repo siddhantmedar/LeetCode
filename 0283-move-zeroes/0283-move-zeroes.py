195b2329ca344bf93aae38aa02b258b59198c2e3
Siddhant Medar <sidmedar21@gmail.com>
2023-07-08 20:07:07 -0500

    Time: 191 ms (28.78%), Space: 18 MB (11.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,26 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-#         stable and non-stable positioning
-
-        l,r = 0,0
-    
-        while r < len(nums):
-            if nums[l]==0 and nums[r]==0:
-                r+=1
-                
-            elif nums[l]==0 and nums[r]!=0:
-                nums[l],nums[r] = nums[r],nums[l]
-                l+=1
-                r+=1
-                
-            else:
-                l+=1
-                r+=1
-                
-                
-    
-# 00->r+=1 ok
-# 01->swap and l+=1,r+=1 ok
-# 10->l+=1,r+=1
-# 11->l+=1,r+=1
+        i,j = 0, 0
+        
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] == 0:
+                j+=1
+            elif nums[i] == 0 and nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+                j+=1
+            elif nums[i] != 0 and nums[j] == 0:
+                i+=1
+                j+=1
+            elif nums[i] != 0 and nums[j] != 0:
+                i+=1
+                j+=1
