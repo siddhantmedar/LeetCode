e29ccbe9a971c98f8a3dd66012d9a9d3e36acee1
Siddhant Medar <sidmedar21@gmail.com>
2023-07-31 21:28:25 -0500

    Time: 2027 ms (75.25%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
-select seller_name from Seller
-where seller_id not in (
-    select seller_id from Orders o
-    where year(o.sale_date) = 2020)
+select seller_name from
+Seller s left join Orders o
+on s.seller_id = o.seller_id and year(sale_date) = '2020'
+where o.seller_id is NULL
 order by seller_name;
