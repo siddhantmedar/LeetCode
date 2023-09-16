d57c75bea1e2e93cd5fbae018661c4c49ee764fe
Siddhant Medar <sidmedar21@gmail.com>
2023-09-16 02:17:59 -0500

    Time: 61 ms (38.00%), Space: 17.6 MB (15.20%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -32,31 +32,3 @@ class Solution:
                     res = min(res, k)
 
         return res
-#         users = defaultdict(list)
-# 	    # It is not necessary to use defaultdict here, we can manually create dictionaries too
-		
-#         for user, time, site in sorted(zip(username, timestamp, website), key = lambda x: (x[0],x[1])):    
-#             users[user].append(site)     # defaultdicts simplify and optimize code
-
-#         patterns = Counter()   # this can also be replaced with a manually created dictionary of counts
-		
-# 		# Get unique 3-sequence (note that website order will automatically be maintained)
-# 		# Note that we take the set of each 3-sequence for each user as they may have repeats
-# 		# For each 3-sequence, count number of users
-		
-#         for user, sites in users.items():
-#             print(sites,  Counter(set(combinations(sites, 3))))
-#             patterns.update(Counter(set(combinations(sites, 3))))     
-		
-# 		# Re-iterating above step for clarity
-# 		# 1. first get all possible 3-sequences combinations(sites, 3)
-# 		# 2. then, count each one once (set)
-# 		# 3. finally, count the number of times we've seen the 3-sequence for every user (patterns.update(Counter)) 
-# 		# - updating a dictionary will update the value for existing keys accordingly (int in this case)
-		
-# 		# An expanded version of the above step is given below.
-			
-#         print("final" , patterns)  # sanity check
-	
-# 		# get most frequent 3-sequence sorted lexicographically
-#         return max(sorted(patterns), key=patterns.get)
