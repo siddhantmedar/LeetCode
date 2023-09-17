c56d02ea786480abec982ad5fc3197d5e2c63fec
Siddhant Medar <sidmedar21@gmail.com>
2023-09-17 18:20:43 -0500

    Time: 32 ms (96.05%), Space: 16.7 MB (22.28%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def solve(open,close,path):
-            if open+close == 2*n:
-                result.append(path)
+        def solve(o,c,path):
+            nonlocal result
+            
+            if o==n and c==n:
+                result.append(path[::])
                 return
             
-            if open < n:
-                solve(open+1,close,path+"(")
-                
-            if close < open:
-                solve(open,close+1, path+")")
+            if o < n:
+                solve(o+1,c,path+"(")
             
+            if o>c:
+                solve(o,c+1,path+")")
+        
         
         result = []
-        
         solve(0,0,"")
+        
+        print(result)
         
         return result
