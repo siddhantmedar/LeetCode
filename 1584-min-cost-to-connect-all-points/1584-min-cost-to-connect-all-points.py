6acd5767c7680a7db42fcd227f64d2eb6de6d12d
Siddhant Medar <sidmedar21@gmail.com>
2023-09-19 20:40:08 -0500

    Time: 1278 ms (54.22%), Space: 99.1 MB (43.77%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,45 +1,30 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        def find(x,y):
-            if parent[(x,y)] != (x,y):
-                parent[(x,y)] = find(parent[(x,y)][0],parent[(x,y)][1])
-                
-            return parent[(x,y)]
-                
-        def union(x,y,i,j):
-            setX = find(x,y)
-            setY = find(i,j)
-            
-            if setX != setY:
-                parent[setY] = setX
-                return True
-            
-            return False
-            
+        visited = set()
         
-        heap = []
-        
-        for (u,v) in points:
-            for (i,j) in points:
-                if (u,v) == (i,j):
-                    continue
-                dist = abs(u-i)+abs(v-j)
-                heap.append((dist,(u,v),(i,j)))
-                
-        heapq.heapify(heap)
-        
-        parent = {(x,y):(x,y) for (x,y) in points}
-        result = 0
+        heap = [(0,tuple(points[0]))]
+
+        n = len(points)
         cnt = 0
         
-        while heap:
-            if cnt == len(points)-1:
-                break
+        result = 0
+        
+        while cnt < n:
+            cost,node = heapq.heappop(heap)
+
+            if node in visited:
+                continue
+
+            visited.add(node)
+
+            result+=cost
             
-            dist,node1,node2 = heapq.heappop(heap)
-            
-            if union(node1[0],node1[1],node2[0],node2[1]):
-                result+=dist
-                cnt+=1
-                
+            cnt+=1
+
+            for nei in points:
+                if tuple(nei) not in visited:
+                    dist = abs(node[0]-nei[0]) + abs(node[1]-nei[1])
+                    heapq.heappush(heap,(dist,tuple(nei)))
+       
+    
         return result
