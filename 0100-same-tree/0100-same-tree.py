ac06f35cfacc331415a0ea54086e881c7ccb6a5b
Siddhant Medar <sidmedar21@gmail.com>
2023-09-24 01:02:04 -0500

    Time: 40 ms (46.90%), Space: 16.3 MB (80.17%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,15 +6,36 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def dfs(p,q):
-            if (p and not q) or (not p and  q):
+#         def dfs(p,q):
+#             if (p and not q) or (not p and  q):
+#                 return False
+            
+#             if not p and not q:
+#                 return True
+            
+#             return p.val==q.val and dfs(p.left,q.left) and dfs(p.right,q.right)
+
+        
+        def bfs(p,q):
+            if (p and not q) or (not p and q):
                 return False
             
-            if not p and not q:
-                return True
+            qq = deque([(p,q)])
             
-            return p.val==q.val and dfs(p.left,q.left) and dfs(p.right,q.right)
+            while qq:
+                n = len(qq)
+                
+                for _ in range(n):
+                    p,q = qq.popleft()
+                    
+                    if (p and not q) or (not p and q) or (p and q and p.val != q.val):
+                        return False
+                    
+                    if p and q:
+                        qq.append((p.left,q.left))
+                        qq.append((p.right,q.right))
+            
+            return True
         
         
-        return dfs(p,q)
-            
+        return bfs(p,q)
