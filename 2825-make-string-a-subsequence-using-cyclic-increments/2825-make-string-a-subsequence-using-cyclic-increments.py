cdb1dd1339f114613e9e665f336df028948d1f39
Siddhant Medar <sidmedar21@gmail.com>
2023-10-18 18:59:18 -0500

    Time: 399 ms (6.94%), Space: 17.7 MB (15.70%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        i,j= 0, 0
+        
+        while i<len(str1) and j<len(str2):
+            print(i,j)
+            nxt = ord(str1[i])
+            
+            if nxt+1 <= 122:
+                nxt+=1
+                nxt = chr(nxt)
+            else:
+                nxt = 'a'
+            
+            if str1[i] == str2[j] or nxt == str2[j]:
+                j+=1
+            i+=1
+        
+        print(j)
+        return j == len(str2)
